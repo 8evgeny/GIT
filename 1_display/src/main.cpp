4782c0d28c232a3f56e530eaae8741384ed61c2f
8evgeny <8evgeny@gmail.com>
2022-02-05 00:39:43 +0300

    ++

--- 1_display/src/main.cpp
+++ 1_display/src/main.cpp
@@ -7,6 +7,7 @@
 #include <chrono>
 #include <thread>
 #include "devices.h"
+#include <cstring>
 
 int main(int argc, char *argv[])
 {
@@ -18,23 +19,34 @@ int main(int argc, char *argv[])
     }
      std::cout<<"Work mode\n";
 
-     auto t1 = std::chrono::steady_clock::now();
-     auto t2 = std::chrono::steady_clock::now();
-     auto int_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+//     auto t1 = std::chrono::steady_clock::now();
+//     auto t2 = std::chrono::steady_clock::now();
+//     auto int_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
 //     while (int_ms.count() < 5000)
      while (1)
      {
-//         display2(500);
-         std::cout << sysCdm("ls").c_str();
-         std::cout<<std::endl<<std::endl;
-         std::this_thread::sleep_for(std::chrono::seconds(2));
          auto v = allDevices();
          for (auto &i:v)
-             std::cout<<i<<std::endl;
+         {
+             std::cout << "device "<< i <<" ping\n";
+             if (strstr(sysCdm("ping -c 3 -f -i 0,2 -n " + i + " | grep \" 0% packet loss\"").c_str(),
+                        "0% packet loss"))
+             {
+                 std::cout << "device "<< i <<" available\n";
+             }
+             else
+             {
+                 std::cout << "device "<< i <<" unavailable\n";
+             }
+             std::this_thread::sleep_for(std::chrono::seconds(1));
+
+         }
 
 
-         t2 = std::chrono::steady_clock::now();
-         int_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+//         display2(500);
+
+//         t2 = std::chrono::steady_clock::now();
+//         int_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
      }
 
 }
