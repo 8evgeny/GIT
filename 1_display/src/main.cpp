75696a4f1ba68dd83804a4a346937caa8de5d363
8evgeny <8evgeny@gmail.com>
2022-02-07 08:16:57 +0300

    +

--- 1_display/src/main.cpp
+++ 1_display/src/main.cpp
@@ -94,6 +94,7 @@ int main(int argc, char *argv[])
         {
             std::cout << "noPingDevice: "<<i <<std::endl;
         }
+
         for (auto &i : noPingNumbersDevices)
         {
             std::cout << "noPingNumberDevice: "<< i <<std::endl;
