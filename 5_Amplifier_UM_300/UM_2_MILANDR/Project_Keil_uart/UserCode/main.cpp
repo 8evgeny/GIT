f3585dae67956ec34cb287aade1fa7c544058cbc
Evgeny Parubets <8evgeny@gmail.com>
2022-03-15 15:54:52 +0300

    Добавил 1 uart

--- 5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart/UserCode/main.cpp
+++ 5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart/UserCode/main.cpp
@@ -15,8 +15,8 @@
 
 
 static PORT_InitTypeDef PortInit;
-static UART_InitTypeDef UART_InitStructure;
-
+static UART_InitTypeDef UART_InitStructure1;
+static UART_InitTypeDef UART_InitStructure2;
 
 int main (void)
 {
@@ -49,6 +49,8 @@ int main (void)
   /* Enables the HSE clock on PORTF */
   RST_CLK_PCLKcmd(RST_CLK_PCLK_PORTF,ENABLE);
 
+  RST_CLK_PCLKcmd(RST_CLK_PCLK_PORTA,ENABLE);
+
   /* Fill PortInit structure*/
   PortInit.PORT_PULL_UP = PORT_PULL_UP_OFF;
   PortInit.PORT_PULL_DOWN = PORT_PULL_DOWN_OFF;
@@ -69,6 +71,16 @@ int main (void)
   PortInit.PORT_Pin = PORT_Pin_0;
   PORT_Init(MDR_PORTF, &PortInit);
 
+  /* Configure PORTF pins 6 (UART1_TX) as output */
+  PortInit.PORT_OE = PORT_OE_OUT;
+  PortInit.PORT_Pin = PORT_Pin_6;
+  PORT_Init(MDR_PORTA, &PortInit);
+
+  /* Configure PORTF pins 5 (UART1_RX) as input */
+  PortInit.PORT_OE = PORT_OE_IN;
+  PortInit.PORT_Pin = PORT_Pin_5;
+  PORT_Init(MDR_PORTA, &PortInit);
+
 
   /* Select HSI/2 as CPU_CLK source*/
   //RST_CLK_CPU_PLLconfig (RST_CLK_CPU_PLLsrcHSIdiv2,0);
@@ -76,22 +88,48 @@ int main (void)
   /* Enables the CPU_CLK clock on UART2 */
   RST_CLK_PCLKcmd(RST_CLK_PCLK_UART2, ENABLE);
 
+  /* Enables the CPU_CLK clock on UART1 */
+  RST_CLK_PCLKcmd(RST_CLK_PCLK_UART1, ENABLE);
+
+
   /* Set the HCLK division factor = 1 for UART2*/
   UART_BRGInit(MDR_UART2, UART_HCLKdiv1);
 
-  /* Initialize UART_InitStructure */
-  UART_InitStructure.UART_BaudRate                = 115000;
-  UART_InitStructure.UART_WordLength              = UART_WordLength8b;
-  UART_InitStructure.UART_StopBits                = UART_StopBits1;
-  UART_InitStructure.UART_Parity                  = UART_Parity_No;
-  UART_InitStructure.UART_FIFOMode                = UART_FIFO_ON;
-  UART_InitStructure.UART_HardwareFlowControl     = UART_HardwareFlowControl_RXE | UART_HardwareFlowControl_TXE;
+  /* Set the HCLK division factor = 1 for UART1*/
+  UART_BRGInit(MDR_UART1, UART_HCLKdiv1);
+
+
+  /* Initialize UART_InitStructure2 */
+  UART_InitStructure2.UART_BaudRate                = 115000;
+  UART_InitStructure2.UART_WordLength              = UART_WordLength8b;
+  UART_InitStructure2.UART_StopBits                = UART_StopBits1;
+  UART_InitStructure2.UART_Parity                  = UART_Parity_No;
+  UART_InitStructure2.UART_FIFOMode                = UART_FIFO_ON;
+  UART_InitStructure2.UART_HardwareFlowControl     = UART_HardwareFlowControl_RXE | UART_HardwareFlowControl_TXE;
 
   /* Configure UART2 parameters*/
-  UART_Init (MDR_UART2,&UART_InitStructure);
+  UART_Init (MDR_UART2,&UART_InitStructure2);
 
-  /* Enables UART2 peripheral */
-  UART_Cmd(MDR_UART2,ENABLE);
+  /* Configure UART1 parameters*/
+  UART_Init (MDR_UART2,&UART_InitStructure2);
+
+  /* Initialize UART_InitStructure1 */
+  UART_InitStructure1.UART_BaudRate                = 115000;
+  UART_InitStructure1.UART_WordLength              = UART_WordLength8b;
+  UART_InitStructure1.UART_StopBits                = UART_StopBits1;
+  UART_InitStructure1.UART_Parity                  = UART_Parity_No;
+  UART_InitStructure1.UART_FIFOMode                = UART_FIFO_ON;
+  UART_InitStructure1.UART_HardwareFlowControl     = UART_HardwareFlowControl_RXE | UART_HardwareFlowControl_TXE;
+
+  /* Configure UART1 parameters*/
+  UART_Init (MDR_UART1,&UART_InitStructure1);
+
+  /* Configure UART1 parameters*/
+  UART_Init (MDR_UART1,&UART_InitStructure1);
+
+  /* Enables UART1 peripheral */
+  UART_Cmd(MDR_UART1,ENABLE);
+
 
   uint8_t tmp_data;
 
@@ -102,8 +140,12 @@ int main (void)
 
     tmp_data = UART_ReceiveData(MDR_UART2);
 
-    UART_SendData(MDR_UART2, tmp_data);
+    UART_SendData(MDR_UART2, tmp_data); // обратно во 2 uart
     while (UART_GetFlagStatus (MDR_UART2, UART_FLAG_TXFE) != SET);
+
+    UART_SendData(MDR_UART1, tmp_data); // обратно в 1 uart
+    while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_TXFE) != SET);
+
   }
 }
 
