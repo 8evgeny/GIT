64746f05650b107979f06c98f9ec833b5f7617f1
Evgeny Parubets <8evgeny@gmail.com>
2022-03-16 11:16:47 +0300

    Template STM32 project

---
+++ 5_Amplifier_UM_300/ZS_2_STM32F103RC6/libwwg/src/usbcdc.d
@@ -0,0 +1,34 @@
+usbcdc.o: usbcdc.c /usr/include/newlib/stdlib.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/stdlib.h /usr/include/newlib/string.h \
+ /usr/include/newlib/sys/string.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book/libopencm3/include/libopencm3/cm3/scb.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book/libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book/libopencm3/include/libopencm3/stm32/rcc.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book/libopencm3/include/libopencm3/stm32/f1/memorymap.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book/libopencm3/include/libopencm3/stm32/f1/rcc.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book/libopencm3/include/libopencm3/stm32/gpio.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book/libopencm3/include/libopencm3/stm32/f1/gpio.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book/libopencm3/include/libopencm3/usb/usbd.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book/libopencm3/include/libopencm3/usb/usbstd.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book/libopencm3/include/libopencm3/usb/cdc.h \
+ rtos/FreeRTOS.h rtos/FreeRTOSConfig.h rtos/projdefs.h rtos/portable.h \
+ rtos/deprecated_definitions.h rtos/portmacro.h rtos/mpu_wrappers.h \
+ rtos/task.h rtos/list.h rtos/queue.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book//rtos/libwwg/include/usbcdc.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book//rtos/libwwg/include/miniprintf.h \
+ /home/evg/SOFT/Github/Arm/STM32_PROJECTS/Beginning_STM32_book//rtos/libwwg/include/getline.h
---
+++ 5_Amplifier_UM_300/ZS_2_STM32F103RC6/main.c
@@ -0,0 +1,46 @@
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+extern void vApplicationStackOverflowHook(
+	xTaskHandle *pxTask,
+	signed portCHAR *pcTaskName);
+
+void
+vApplicationStackOverflowHook(
+  xTaskHandle *pxTask __attribute((unused)),
+  signed portCHAR *pcTaskName __attribute((unused))
+) {
+	for(;;);	// Loop forever here..
+}
+
+static void
+task1(void *args __attribute((unused))) {
+
+	for (;;) {
+        gpio_toggle(GPIOC,GPIO6);
+        vTaskDelay(pdMS_TO_TICKS(200));
+	}
+}
+
+int
+main(void) {
+
+//    rcc_clock_setup_in_hse_8mhz_out_72mhz(); // For "blue pill"
+    rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+
+	rcc_periph_clock_enable(RCC_GPIOC);
+
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO6);
+
+    xTaskCreate(task1, "LED", 100, NULL, configMAX_PRIORITIES-1, NULL);
+	vTaskStartScheduler();
+
+	for (;;);
+
+	return 0;
+}
+
+// End
