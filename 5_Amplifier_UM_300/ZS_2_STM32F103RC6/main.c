c9c203fc0d20161f3cdc0e4a55bb0192e060e154
Evgeny Parubets <8evgeny@gmail.com>
2022-03-16 11:26:33 +0300

    blink 2 ports

--- 5_Amplifier_UM_300/ZS_2_STM32F103RC6/main.c
+++ 5_Amplifier_UM_300/ZS_2_STM32F103RC6/main.c
@@ -17,12 +17,23 @@ vApplicationStackOverflowHook(
 }
 
 static void
-task1(void *args __attribute((unused))) {
+task1(void *args __attribute((unused)))
+{
 
 	for (;;) {
-        gpio_toggle(GPIOC,GPIO6);
+        gpio_toggle(GPIOC, GPIO6);
         vTaskDelay(pdMS_TO_TICKS(200));
 	}
+}
+
+static void
+task2(void *args __attribute((unused)))
+{
+
+    for (;;) {
+        gpio_toggle(GPIOB, GPIO14);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
 }
 
 int
@@ -32,10 +43,13 @@ main(void) {
     rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
 
 	rcc_periph_clock_enable(RCC_GPIOC);
+    rcc_periph_clock_enable(RCC_GPIOB);
 
     gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO6);
+    gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO14);
 
-    xTaskCreate(task1, "LED", 100, NULL, configMAX_PRIORITIES-1, NULL);
+    xTaskCreate(task1, "LED1", 100, NULL, configMAX_PRIORITIES-1, NULL);
+    xTaskCreate(task2, "LED2", 100, NULL, configMAX_PRIORITIES-1, NULL);
 	vTaskStartScheduler();
 
 	for (;;);
