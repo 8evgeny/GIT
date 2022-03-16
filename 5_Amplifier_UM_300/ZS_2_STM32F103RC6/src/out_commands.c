19e5939d6e87645e0772baded17b3b3757118a39
Evgeny Parubets <8evgeny@gmail.com>
2022-03-16 16:37:49 +0300

    Выходы +

--- 5_Amplifier_UM_300/ZS_2_STM32F103RC6/src/out_commands.c
+++ 5_Amplifier_UM_300/ZS_2_STM32F103RC6/src/out_commands.c
@@ -1,32 +1,28 @@
 #include "main.h"
 
-void setImpedanceRele()  { gpio_set(GPIOA, GPIO8);}
-void resetImpedanceRele(){ gpio_clear(GPIOA, GPIO8);}
+void setImpedanceRele(bool set) { if(set) gpio_set(GPIOA, GPIO8);  else gpio_clear(GPIOA, GPIO8);}
+void setReleTr1(bool set) { if(set) gpio_set(GPIOA, GPIO9);  else gpio_clear(GPIOA, GPIO9);}
+void setReleTr2(bool set) { if(set) gpio_set(GPIOA, GPIO10);  else gpio_clear(GPIOA, GPIO10);}
+void setReleTr3(bool set) { if(set) gpio_set(GPIOA, GPIO11);  else gpio_clear(GPIOA, GPIO11);}
+void setReleTr4(bool set) { if(set) gpio_set(GPIOA, GPIO12);  else gpio_clear(GPIOA, GPIO12);}
 
-void setReleTr1()   { gpio_set(GPIOA, GPIO9);}
-void resetReleTr1() { gpio_clear(GPIOA, GPIO9);}
-void setReleTr2()   { gpio_set(GPIOA, GPIO10);}
-void resetReleTr2() { gpio_clear(GPIOA, GPIO10);}
-void setReleTr3()   { gpio_set(GPIOA, GPIO11);}
-void resetReleTr3() { gpio_clear(GPIOA, GPIO11);}
-void setReleTr4()   { gpio_set(GPIOA, GPIO12);}
-void resetReleTr4() { gpio_clear(GPIOA, GPIO12);}
+void setReadyLed(bool set) { if(set) gpio_set(GPIOB, GPIO8);  else gpio_clear(GPIOB, GPIO8);}
+void setUpr1(bool set) { if(set) gpio_set(GPIOB, GPIO1);  else gpio_clear(GPIOB, GPIO1);}
+void setUpr2(bool set) { if(set) gpio_set(GPIOB, GPIO2);  else gpio_clear(GPIOB, GPIO2);}
 
-void setReadyLed()  { gpio_set(GPIOB, GPIO0);}
-void resetReadyLed(){ gpio_clear(GPIOB, GPIO0);}
-void setUpr1()      { gpio_set(GPIOB, GPIO1);}
-void resetUpr1()    { gpio_clear(GPIOB, GPIO1);}
-void setUpr2()      { gpio_set(GPIOB, GPIO2);}
-void resetUpr2()    { gpio_clear(GPIOB, GPIO2);}
+void setLedShortOut(bool set) { if(set) gpio_set(GPIOB, GPIO10);  else gpio_clear(GPIOB, GPIO10);}
+void setLedBreakOut(bool set) { if(set) gpio_set(GPIOB, GPIO11);  else gpio_clear(GPIOB, GPIO11);}
+void setLedOvercutOut(bool set) { if(set) gpio_set(GPIOB, GPIO12);  else gpio_clear(GPIOB, GPIO12);}
+void setLedOverheatOut(bool set) { if(set) gpio_set(GPIOB, GPIO13);  else gpio_clear(GPIOB, GPIO13);}
+void setErrorRele(bool set) { if(set) gpio_set(GPIOB, GPIO14);  else gpio_clear(GPIOB, GPIO14);}
+void setReleLine1(bool set) { if(set) gpio_set(GPIOB, GPIO15);  else gpio_clear(GPIOB, GPIO15);}
 
-void setLedShortOut()     { gpio_set(GPIOB, GPIO10);}
-void resetLedShortOut()   { gpio_clear(GPIOB, GPIO10);}
-void setLedBreakOut()     { gpio_set(GPIOB, GPIO11);}
-void resetLedBreakOut()   { gpio_clear(GPIOB, GPIO11);}
-void setLedOvercutOut()   { gpio_set(GPIOB, GPIO12);}
-void resetLedOvercutOut() { gpio_clear(GPIOB, GPIO12);}
-void setLedOverheatOut()  { gpio_set(GPIOB, GPIO13);}
-void resetLedOverheatOut(){ gpio_clear(GPIOB, GPIO13);}
+void setRele24V(bool set) { if(set) gpio_set(GPIOC, GPIO9);  else gpio_clear(GPIOC, GPIO9);}
 
-void fanOn()  { gpio_clear(GPIOB, GPIO9);}//Инверсия
-void fanOff(){ gpio_set(GPIOB, GPIO9);}
+void setFan(bool set) { if(set) gpio_set(GPIOB, GPIO9);  else gpio_clear(GPIOB, GPIO9);}
+//void setPowerOut(bool set) { if(set) gpio_set(GPIOB, GPIO8);  else gpio_clear(GPIOB, GPIO8);} //на UART переходит
+void setPowerOut(bool set)
+{
+    //Передача команды через UART
+
+}
