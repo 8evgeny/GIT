791b33b768df0df5a2e4e0b5f8d4f9db7c89d56c
Evgeny Parubets <8evgeny@gmail.com>
2022-03-18 14:43:19 +0300

    uart4 режим RX TX

--- 5_Amplifier_UM_300/ZS_2_STM32F103RC6/src/main.c
+++ 5_Amplifier_UM_300/ZS_2_STM32F103RC6/src/main.c
@@ -179,13 +179,13 @@ testUART2(void *args)
 static void
 uart_setup()
 {
-//    usart_set_baudrate(UART4,115200);
-//    usart_set_databits(UART4,8);
-//    usart_set_stopbits(UART4,USART_STOPBITS_1);
-//    usart_set_mode(UART4,USART_MODE_TX);
-//    usart_set_parity(UART4,USART_PARITY_NONE);
-//    usart_set_flow_control(UART4,USART_FLOWCONTROL_NONE);
-//    usart_enable(UART4);
+    usart_set_baudrate(UART4,115200);
+    usart_set_databits(UART4,8);
+    usart_set_stopbits(UART4,USART_STOPBITS_1);
+    usart_set_mode(UART4,USART_MODE_TX_RX);
+    usart_set_parity(UART4,USART_PARITY_NONE);
+    usart_set_flow_control(UART4,USART_FLOWCONTROL_NONE);
+    usart_enable(UART4);
 
     if(!uart4) open_uart(1, 115200, "8N1", "rw", 0, 0); //Описание в теле функции
     if(uart4) open_uart(4, 115200, "8N1", "rw", 0, 0);
