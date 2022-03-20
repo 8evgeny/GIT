c03f98644572bf17a0fc4230bdf84ab00b3db733
8evgeny <8evgeny@gmail.com>
2022-03-20 17:31:01 +0300

    ++

--- 5_Amplifier_UM_300/ZS_2_STM32F103RC6/src/out_commands.c
+++ 5_Amplifier_UM_300/ZS_2_STM32F103RC6/src/out_commands.c
@@ -31,6 +31,6 @@ void setPowerOut(bool set)
 //Свободные пины
 void setTestLed1(bool set) { if(set) gpio_set(GPIOC, GPIO15);  else gpio_clear(GPIOC, GPIO15);}
 void setTestLed2(bool set) { if(set) gpio_set(GPIOC, GPIO14);  else gpio_clear(GPIOC, GPIO14);}
-void setTestLed3(bool set) { if(set) gpio_set(GPIOC, GPIO4);  else gpio_clear(GPIOC, GPIO4);}
-void setTestLed4(bool set) { if(set) gpio_set(GPIOC, GPIO5);  else gpio_clear(GPIOC, GPIO5);}
+void setTestLed3(bool set) { if(set) gpio_set(GPIOC, GPIO5);  else gpio_clear(GPIOC, GPIO5);}
+void setTestLed4(bool set) { if(set) gpio_set(GPIOC, GPIO4);  else gpio_clear(GPIOC, GPIO4);}
 
