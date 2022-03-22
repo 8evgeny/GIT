6bc0d3d61cc915d8ba46694a68216a5f2d761100
8evgeny <8evgeny@gmail.com>
2022-03-22 08:04:38 +0300

    fix errrors

--- 5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart/UserCode/main.cpp
+++ 5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart/UserCode/main.cpp
@@ -14,7 +14,8 @@
 //#include "MDR32F9Qx_eeprom.h"
 
 
-static PORT_InitTypeDef PortInit;
+static PORT_InitTypeDef PortInitUART1;
+static PORT_InitTypeDef PortInitUART2;
 static UART_InitTypeDef UART_InitStructure1;
 static UART_InitTypeDef UART_InitStructure2;
 
@@ -33,7 +34,7 @@ int main (void)
    }
 
    /* Enables the RST_CLK_PCLK_EEPROM */
-   RST_CLK_PCLKcmd(RST_CLK_PCLK_EEPROM, ENABLE);
+//   RST_CLK_PCLKcmd(RST_CLK_PCLK_EEPROM, ENABLE);
    /* Sets the code latency value */
 
 //   EEPROM_SetLatency(EEPROM_Latency_3);
@@ -43,60 +44,83 @@ int main (void)
    /* Set CPUClk Prescaler */
    RST_CLK_CPUclkPrescaler(RST_CLK_CPUclkDIV1);
 
-   /* Select the CPU clock source */
-   RST_CLK_CPUclkSelection(RST_CLK_CPUclkCPU_C3);
+  /* Select the CPU clock source */
+  RST_CLK_CPUclkSelection(RST_CLK_CPUclkCPU_C3);
+
+  /* Enables the HSE clock on PORTB */
+  RST_CLK_PCLKcmd(RST_CLK_PCLK_PORTB,ENABLE);
 
   /* Enables the HSE clock on PORTF */
   RST_CLK_PCLKcmd(RST_CLK_PCLK_PORTF,ENABLE);
 
-  RST_CLK_PCLKcmd(RST_CLK_PCLK_PORTA,ENABLE);
+  /* Fill PortInit UART1 structure*/
+  PortInitUART1.PORT_PULL_UP = PORT_PULL_UP_OFF;
+  PortInitUART1.PORT_PULL_DOWN = PORT_PULL_DOWN_OFF;
+  PortInitUART1.PORT_PD_SHM = PORT_PD_SHM_OFF;
+  PortInitUART1.PORT_PD = PORT_PD_DRIVER;
+  PortInitUART1.PORT_GFEN = PORT_GFEN_OFF;
+  PortInitUART1.PORT_FUNC = PORT_FUNC_ALTER;
+  PortInitUART1.PORT_SPEED = PORT_SPEED_MAXFAST;
+  PortInitUART1.PORT_MODE = PORT_MODE_DIGITAL;
 
-  /* Fill PortInit structure*/
-  PortInit.PORT_PULL_UP = PORT_PULL_UP_OFF;
-  PortInit.PORT_PULL_DOWN = PORT_PULL_DOWN_OFF;
-  PortInit.PORT_PD_SHM = PORT_PD_SHM_OFF;
-  PortInit.PORT_PD = PORT_PD_DRIVER;
-  PortInit.PORT_GFEN = PORT_GFEN_OFF;
-  PortInit.PORT_FUNC = PORT_FUNC_OVERRID;  //ОШИБКА
-  PortInit.PORT_SPEED = PORT_SPEED_MAXFAST;
-  PortInit.PORT_MODE = PORT_MODE_DIGITAL;
+  /* Configure PORTB pins 5 (UART1_TX) as output  */
+  PortInitUART1.PORT_OE = PORT_OE_OUT;
+  PortInitUART1.PORT_Pin = PORT_Pin_5;
+  PORT_Init(MDR_PORTB, &PortInitUART1);
+  /* Configure PORTB pins 6 (UART1_RX) as input  */
+  PortInitUART1.PORT_OE = PORT_OE_IN;
+  PortInitUART1.PORT_Pin = PORT_Pin_6;
+  PORT_Init(MDR_PORTB, &PortInitUART1);
 
+
+
+  /* Fill PortInit UART2 structure*/
+  PortInitUART2.PORT_PULL_UP = PORT_PULL_UP_OFF;
+  PortInitUART2.PORT_PULL_DOWN = PORT_PULL_DOWN_OFF;
+  PortInitUART2.PORT_PD_SHM = PORT_PD_SHM_OFF;
+  PortInitUART2.PORT_PD = PORT_PD_DRIVER;
+  PortInitUART2.PORT_GFEN = PORT_GFEN_OFF;
+  PortInitUART2.PORT_FUNC = PORT_FUNC_OVERRID;
+  PortInitUART2.PORT_SPEED = PORT_SPEED_MAXFAST;
+  PortInitUART2.PORT_MODE = PORT_MODE_DIGITAL;
+
+  //UART2 использую для прошивки в Linux
   /* Configure PORTF pins 1 (UART2_TX) as output */
-  PortInit.PORT_OE = PORT_OE_OUT;
-  PortInit.PORT_Pin = PORT_Pin_1;
-  PORT_Init(MDR_PORTF, &PortInit);
-
+  PortInitUART2.PORT_OE = PORT_OE_OUT;
+  PortInitUART2.PORT_Pin = PORT_Pin_1;
+  PORT_Init(MDR_PORTF, &PortInitUART2);
   /* Configure PORTF pins 0 (UART2_RX) as input */
-  PortInit.PORT_OE = PORT_OE_IN;
-  PortInit.PORT_Pin = PORT_Pin_0;
-  PORT_Init(MDR_PORTF, &PortInit);
-
-  /* Configure PORTA pins 6 (UART1_TX) as output ?????????? */
-  PortInit.PORT_OE = PORT_OE_OUT;
-  PortInit.PORT_Pin = PORT_Pin_6;
-  PORT_Init(MDR_PORTA, &PortInit);
-
-  /* Configure PORTA pins 5 (UART1_RX) as input ?????????? */
-  PortInit.PORT_OE = PORT_OE_IN;
-  PortInit.PORT_Pin = PORT_Pin_5;
-  PORT_Init(MDR_PORTA, &PortInit);
+  PortInitUART2.PORT_OE = PORT_OE_IN;
+  PortInitUART2.PORT_Pin = PORT_Pin_0;
+  PORT_Init(MDR_PORTF, &PortInitUART2);
 
 
   /* Select HSI/2 as CPU_CLK source*/
   //RST_CLK_CPU_PLLconfig (RST_CLK_CPU_PLLsrcHSIdiv2,0);
 
-  /* Enables the CPU_CLK clock on UART2 */
-  RST_CLK_PCLKcmd(RST_CLK_PCLK_UART2, ENABLE);
-
   /* Enables the CPU_CLK clock on UART1 */
   RST_CLK_PCLKcmd(RST_CLK_PCLK_UART1, ENABLE);
 
+  /* Enables the CPU_CLK clock on UART2 */
+  RST_CLK_PCLKcmd(RST_CLK_PCLK_UART2, ENABLE);
+
+  /* Set the HCLK division factor = 1 for UART1*/
+  UART_BRGInit(MDR_UART1, UART_HCLKdiv1);
 
   /* Set the HCLK division factor = 1 for UART2*/
   UART_BRGInit(MDR_UART2, UART_HCLKdiv1);
 
-  /* Set the HCLK division factor = 1 for UART1*/
-  UART_BRGInit(MDR_UART1, UART_HCLKdiv1);
+  /* Initialize UART_InitStructure1 */
+  UART_InitStructure1.UART_BaudRate                = 115000;
+  UART_InitStructure1.UART_WordLength              = UART_WordLength8b;
+  UART_InitStructure1.UART_StopBits                = UART_StopBits1;
+  UART_InitStructure1.UART_Parity                  = UART_Parity_No;
+  UART_InitStructure1.UART_FIFOMode                = UART_FIFO_ON;
+  UART_InitStructure1.UART_HardwareFlowControl     = UART_HardwareFlowControl_RXE | UART_HardwareFlowControl_TXE;
+  /* Configure UART1 parameters*/
+  UART_Init (MDR_UART1,&UART_InitStructure1);
+  /* Enables UART1 peripheral */
+  UART_Cmd(MDR_UART1,ENABLE);
 
 
   /* Initialize UART_InitStructure2 */
@@ -106,33 +130,12 @@ int main (void)
   UART_InitStructure2.UART_Parity                  = UART_Parity_No;
   UART_InitStructure2.UART_FIFOMode                = UART_FIFO_ON;
   UART_InitStructure2.UART_HardwareFlowControl     = UART_HardwareFlowControl_RXE | UART_HardwareFlowControl_TXE;
-
   /* Configure UART2 parameters*/
   UART_Init (MDR_UART2,&UART_InitStructure2);
+  /* Enables UART2 peripheral */
+  UART_Cmd(MDR_UART2,ENABLE);
 
-  /* Configure UART1 parameters*/
-  UART_Init (MDR_UART2,&UART_InitStructure2);
-
-  /* Initialize UART_InitStructure1 */
-  UART_InitStructure1.UART_BaudRate                = 115000;
-  UART_InitStructure1.UART_WordLength              = UART_WordLength8b;
-  UART_InitStructure1.UART_StopBits                = UART_StopBits1;
-  UART_InitStructure1.UART_Parity                  = UART_Parity_No;
-  UART_InitStructure1.UART_FIFOMode                = UART_FIFO_ON;
-  UART_InitStructure1.UART_HardwareFlowControl     = UART_HardwareFlowControl_RXE | UART_HardwareFlowControl_TXE;
-
-  /* Configure UART1 parameters*/
-  UART_Init (MDR_UART1,&UART_InitStructure1);
-
-  /* Configure UART1 parameters*/
-  UART_Init (MDR_UART1,&UART_InitStructure1);
-
-  /* Enables UART1 peripheral */
-  UART_Cmd(MDR_UART1,ENABLE);
-
-
-  uint8_t tmp_data1;
-  uint8_t tmp_data2;
+  uint8_t tmp_data;
 
   while (1)
   {
@@ -143,9 +146,9 @@ int main (void)
 //    while (UART_GetFlagStatus (MDR_UART2, UART_FLAG_TXFE) != SET);
 
     while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_RXFE) == SET);
-    tmp_data1 = UART_ReceiveData(MDR_UART1);
+    tmp_data = UART_ReceiveData(MDR_UART1);
 
-    UART_SendData(MDR_UART1, tmp_data1);
+    UART_SendData(MDR_UART1, tmp_data);
     while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_TXFE) != SET);
  
 
