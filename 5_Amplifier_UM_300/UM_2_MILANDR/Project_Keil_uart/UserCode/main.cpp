94083f31c0e298ac4925a5f4306238b4d79452f4
Evgeny Parubets <8evgeny@gmail.com>
2022-03-24 15:52:43 +0300

    ++

--- 5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart/UserCode/main.cpp
+++ 5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart/UserCode/main.cpp
@@ -13,20 +13,22 @@ int main (void)
 
     while (1)
     {
-        auto t1 = Time::now();
-        fsec fs = t1 - t0;
-        mcs d = std::chrono::duration_cast<mcs>(fs);
+//        auto t1 = Time::now();
+//        fsec fs = t1 - t0;
+//        mcs d = std::chrono::duration_cast<mcs>(fs);
 
-        if (d.count() >1000)
-        {
+//        if (d.count() >1000)
+//        {
             while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_RXFE) == SET);  //ждем пока не не установиться флаг по приему байта
             uint8_t ReciveByte = UART_ReceiveData(MDR_UART1);               //считываем принятый байт
-    //        delay(0xFF);
-    //        PORT_ResetBits(MDR_PORTC, PORT_Pin_1);
             UART_SendData(MDR_UART1, checkReceivedByte(ReciveByte));
             while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_TXFE) != SET);
-            t0 = t1;
-        }
+
+            PORT_SetBits(MDR_PORTC, PORT_Pin_1);
+            delay(0xFFFF);
+            PORT_ResetBits(MDR_PORTC, PORT_Pin_1);
+//            t0 = t1;
+//        }
 
 
  
