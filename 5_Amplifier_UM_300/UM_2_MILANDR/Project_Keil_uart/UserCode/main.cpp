e87bb87c3c7b243e0a2f44581b4e84bb7e53a412
Evgeny Parubets <8evgeny@gmail.com>
2022-03-25 14:00:47 +0300

    3

--- 5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart/UserCode/main.cpp
+++ 5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart/UserCode/main.cpp
@@ -98,14 +98,14 @@ int main (void)
     {
 #ifdef VARIANT_NOINT
 
-//        while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_RXFE) == SET);  //ждем пока не не установиться флаг по приему байта
-        if (UART_GetFlagStatus (MDR_UART1, UART_FLAG_RXFE) == SET)
-        {
-            ReciveByte = UART_ReceiveData(MDR_UART1);               //считываем принятый байт
-            UART_SendData(MDR_UART1, checkReceivedByte(ReciveByte));
-            while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_TXFE) != SET);
-        }
+        while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_RXFE) == SET);  //ждем пока не не установиться флаг по приему байта
+//        if (UART_GetFlagStatus (MDR_UART1, UART_FLAG_RXFE) == SET)
+//        {
 
+//        }
+        ReciveByte = UART_ReceiveData(MDR_UART1);               //считываем принятый байт
+        UART_SendData(MDR_UART1, checkReceivedByte(ReciveByte));
+        while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_TXFE) != SET);
 
         PORT_SetBits(MDR_PORTC, PORT_Pin_1);
         delay(0x6FFFF);
