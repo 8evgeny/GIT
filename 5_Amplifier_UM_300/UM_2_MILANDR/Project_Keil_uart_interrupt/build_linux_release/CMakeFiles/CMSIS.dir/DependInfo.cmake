2526f97e6be762978f8efb76d99315d269998ec2
8evgeny <8evgeny@gmail.com>
2022-03-26 21:22:02 +0300

    new test project

---
+++ 5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart_interrupt/build_linux_release/CMakeFiles/CMSIS.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/CMSIS/MDR32Fx/DeviceSupport/MDR1986VE9x/startup/arm/system_MDR32F9Qx.c" "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart_interrupt/build_linux_release/CMakeFiles/CMSIS.dir/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/CMSIS/MDR32Fx/DeviceSupport/MDR1986VE9x/startup/arm/system_MDR32F9Qx.c.o"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/MDR32F9Qx_iwdg.c" "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart_interrupt/build_linux_release/CMakeFiles/CMSIS.dir/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/MDR32F9Qx_iwdg.c.o"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/MDR32F9Qx_port.c" "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart_interrupt/build_linux_release/CMakeFiles/CMSIS.dir/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/MDR32F9Qx_port.c.o"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/MDR32F9Qx_rst_clk.c" "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart_interrupt/build_linux_release/CMakeFiles/CMSIS.dir/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/MDR32F9Qx_rst_clk.c.o"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/MDR32F9Qx_timer.c" "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart_interrupt/build_linux_release/CMakeFiles/CMSIS.dir/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/MDR32F9Qx_timer.c.o"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/MDR32F9Qx_uart.c" "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart_interrupt/build_linux_release/CMakeFiles/CMSIS.dir/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/MDR32F9Qx_uart.c.o"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/MDR32F9Qx_usb.c" "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart_interrupt/build_linux_release/CMakeFiles/CMSIS.dir/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/MDR32F9Qx_usb.c.o"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/USB_Library/MDR32F9Qx_usb_CDC.c" "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart_interrupt/build_linux_release/CMakeFiles/CMSIS.dir/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/USB_Library/MDR32F9Qx_usb_CDC.c.o"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/USB_Library/MDR32F9Qx_usb_device.c" "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart_interrupt/build_linux_release/CMakeFiles/CMSIS.dir/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src/USB_Library/MDR32F9Qx_usb_device.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Keil_uart_interr/../MDR1986BExx/2.0.3/Libraries/CMSIS/MDR32Fx/DeviceSupport/MDR1986VE9x/inc"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Keil_uart_interr/../MDR1986BExx/2.0.3/Libraries/CMSIS/MDR32Fx/DeviceSupport/MDR1986VE9x/startup/arm"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Keil_uart_interr/../Milandr_MCU_1986x_Standard_Peripherals_Library/CMSIS/CM3/DeviceSupport/MDR32F9Qx/startup/gcc"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Keil_uart_interr/../MDR1986BExx/2.0.3/Libraries/CMSIS/MDR32Fx/CoreSupport/CM3"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Keil_uart_interr/../MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Keil_uart_interr/../MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/inc"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Keil_uart_interr/../MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/inc/USB_Library"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Keil_uart_interr/../MDR1986BExx/2.0.3/Libraries/MDR32Fx/inc"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Keil_uart_interr/../MDR1986BExx/2.0.3/Libraries/SPL/MDR32Fx/src"
+  "/home/evg/toolchain/gcc-arm-none-eabi-new/arm-none-eabi"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Keil_uart_interr/UserCode"
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Keil_uart_interr/."
+  "/home/evg/SOFT/Github/GIT/5_Amplifier_UM_300/UM_2_MILANDR/Keil_uart_interr/rtos/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
