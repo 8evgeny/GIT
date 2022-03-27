9b4f4d774ad22cd52172be5c97e3abe9511da20b
8evgeny <8evgeny@gmail.com>
2022-03-27 09:06:57 +0300

    ++

---
+++ 5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart_rtos/build_linux_release/CMakeFiles/milandr_uart_rtos.elf.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/milandr_uart_rtos.elf.dir/UserCode/SPL_src/MDR32F9Qx_iwdg.c.o"
+  "CMakeFiles/milandr_uart_rtos.elf.dir/UserCode/SPL_src/MDR32F9Qx_port.c.o"
+  "CMakeFiles/milandr_uart_rtos.elf.dir/UserCode/SPL_src/MDR32F9Qx_rst_clk.c.o"
+  "CMakeFiles/milandr_uart_rtos.elf.dir/UserCode/SPL_src/MDR32F9Qx_timer.c.o"
+  "CMakeFiles/milandr_uart_rtos.elf.dir/UserCode/SPL_src/MDR32F9Qx_usb.c.o"
+  "CMakeFiles/milandr_uart_rtos.elf.dir/UserCode/check_receive_data.c.o"
+  "CMakeFiles/milandr_uart_rtos.elf.dir/UserCode/init_GPIO.c.o"
+  "CMakeFiles/milandr_uart_rtos.elf.dir/UserCode/init_UART.c.o"
+  "CMakeFiles/milandr_uart_rtos.elf.dir/UserCode/main.c.o"
+  "milandr_uart_rtos.elf"
+  "milandr_uart_rtos.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/milandr_uart_rtos.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
