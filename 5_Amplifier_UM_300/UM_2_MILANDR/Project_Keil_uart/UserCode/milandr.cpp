17355d7b0bc0754170909af141bdc85a46775474
8evgeny <8evgeny@gmail.com>
2022-03-28 21:10:17 +0300

    class Milandr

--- 5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart/UserCode/milandr.cpp
+++ 5_Amplifier_UM_300/UM_2_MILANDR/Project_Keil_uart/UserCode/milandr.cpp
@@ -136,7 +136,6 @@ void Milandr::initUART()
 
 }
 
-
 bool Milandr::getMAINPOWER()
 {
     if (1 == PORT_ReadInputDataBit(MDR_PORTA, PORT_Pin_0))
@@ -180,4 +179,28 @@ bool Milandr::getPOW_READY()
         return true;
     }
     return false;
+}
+
+void Milandr::setGAIN_UPR (bool set)
+{
+    if(set)  PORT_SetBits(MDR_PORTA, PORT_Pin_2);
+    if(!set) PORT_ResetBits(MDR_PORTA, PORT_Pin_2);
+}
+
+void Milandr::setCOMP_UPR (bool set)
+{
+    if(set)  PORT_SetBits(MDR_PORTA, PORT_Pin_3);
+    if(!set) PORT_ResetBits(MDR_PORTA, PORT_Pin_3);
+}
+
+void Milandr::setFAN_ON (bool set)
+{
+    if(set)  PORT_SetBits(MDR_PORTA, PORT_Pin_7);
+    if(!set) PORT_ResetBits(MDR_PORTA, PORT_Pin_7);
+}
+
+void Milandr::setRESET_UPR (bool set)
+{
+    if(set)  PORT_SetBits(MDR_PORTB, PORT_Pin_8);
+    if(!set) PORT_ResetBits(MDR_PORTB, PORT_Pin_8);
 }
