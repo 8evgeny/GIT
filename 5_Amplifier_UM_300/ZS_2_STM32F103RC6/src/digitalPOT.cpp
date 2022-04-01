d1158b980c812d60e0a6accba7d0804f65d30e96
Evgeny Parubets <8evgeny@gmail.com>
2022-04-01 13:27:40 +0300

    +++

--- 5_Amplifier_UM_300/ZS_2_STM32F103RC6/src/digitalPOT.cpp
+++ 5_Amplifier_UM_300/ZS_2_STM32F103RC6/src/digitalPOT.cpp
@@ -17,9 +17,8 @@ void digitaPOT(void *args)
 
             char buf[10];
             sprintf(buf, "%d", ii);
+//            stringTo_diagnostic_Usart1(buf);
 //            send_to_POT(ii);
-            stringTo_diagnostic_Usart1(buf);
-
         }
 
 
