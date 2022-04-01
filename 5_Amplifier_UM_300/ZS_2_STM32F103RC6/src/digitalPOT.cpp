ccf15b218a968592138aef8ef97986be0a9d827a
Evgeny Parubets <8evgeny@gmail.com>
2022-04-01 13:25:30 +0300

    Фиксирую перед проверкой i2C

--- 5_Amplifier_UM_300/ZS_2_STM32F103RC6/src/digitalPOT.cpp
+++ 5_Amplifier_UM_300/ZS_2_STM32F103RC6/src/digitalPOT.cpp
@@ -11,19 +11,18 @@ void digitaPOT(void *args)
     for(;;)
     {
 
-//        for (ii = 0; ii < 127 ; ++ii)
-//        {
-//            char buf[10];
-//            sprintf(buf, "%d", ii);
+        for (ii = 0; ii < 127 ; ++ii)
+        {
+            vTaskDelay(pdMS_TO_TICKS(200));
+
+            char buf[10];
+            sprintf(buf, "%d", ii);
 //            send_to_POT(ii);
-////            stringToLcd("buf");
-////            stringToLcd(buf);
+            stringTo_diagnostic_Usart1(buf);
 
-//stringTo_diagnostic_Usart1("test\n\r");
+        }
 
-//        }
 
-//stringTo_diagnostic_Usart1("test\n\r");
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 
