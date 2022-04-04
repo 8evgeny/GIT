f8eece138db61f0bf8d9f9e20b4a5f2d3b62951e
Evgeny Parubets <8evgeny@gmail.com>
2022-04-04 18:04:34 +0300

    ++

---
+++ 5_Amplifier_UM_300/ZS_2_STM32F103R8T6_no_rtos/rtos/port.d
@@ -0,0 +1,5 @@
+rtos/port.o: rtos/port.c rtos/FreeRTOS.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h FreeRTOSConfig.h \
+ rtos/projdefs.h rtos/portable.h rtos/deprecated_definitions.h \
+ rtos/portmacro.h rtos/mpu_wrappers.h rtos/task.h rtos/list.h
