d51929f5d11a757ffef4828230dfe76d994c08f5
Evgeny Parubets <8evgeny@gmail.com>
2022-04-05 10:39:15 +0300

    ++++

--- 5_Amplifier_UM_300/ZS_2_STM32F103R8T6_no_rtos/src/out_commands.c
+++ 5_Amplifier_UM_300/ZS_2_STM32F103R8T6_no_rtos/src/out_commands.c
@@ -1,35 +1,35 @@
 #include "main.h"
 
-void setImpedanceRele(bool set) { if(set) gpio_set(GPIOA, GPIO8);  else gpio_clear(GPIOA, GPIO8);}
+void setImpedanceRele(bool set) { if(set) gpio_clear(GPIOA, GPIO8);  else gpio_set(GPIOA, GPIO8);}
 
-void setReleTr1(bool set) { if(set) gpio_set(GPIOA, GPIO9);  else gpio_clear(GPIOA, GPIO9);}    //LED Трансляция1
-void setReleTr2(bool set) { if(set) gpio_set(GPIOA, GPIO10);  else gpio_clear(GPIOA, GPIO10);}  //LED Трансляция2
-void setReleTr3(bool set) { if(set) gpio_set(GPIOA, GPIO11);  else gpio_clear(GPIOA, GPIO11);}  //LED Трансляция3
-void setReleTr4(bool set) { if(set) gpio_set(GPIOA, GPIO12);  else gpio_clear(GPIOA, GPIO12);}  //LED Трансляция4
+void setReleTr1(bool set) { if(set) gpio_clear(GPIOA, GPIO9);  else gpio_set(GPIOA, GPIO9);}    //LED Трансляция1
+void setReleTr2(bool set) { if(set) gpio_clear(GPIOA, GPIO10);  else gpio_set(GPIOA, GPIO10);}  //LED Трансляция2
+void setReleTr3(bool set) { if(set) gpio_clear(GPIOA, GPIO11);  else gpio_set(GPIOA, GPIO11);}  //LED Трансляция3
+void setReleTr4(bool set) { if(set) gpio_clear(GPIOA, GPIO12);  else gpio_set(GPIOA, GPIO12);}  //LED Трансляция4
 
-void setReadyLed(bool set) { if(set) gpio_clear(GPIOB, GPIO0);  else gpio_set(GPIOB, GPIO0);}   //LED Готовность
-void setUpr1(bool set) { if(set) gpio_set(GPIOB, GPIO1);  else gpio_clear(GPIOB, GPIO1);}       //UPR 078 микширование не задействованы
-void setUpr2(bool set) { if(set) gpio_set(GPIOB, GPIO2);  else gpio_clear(GPIOB, GPIO2);}       //UPR ST  микширование не задействованы
+void setReadyLed(bool set) { if(set) gpio_set(GPIOB, GPIO0);  else gpio_clear(GPIOB, GPIO0);}   //LED Готовность
+void setUpr1(bool set) { if(set) gpio_clear(GPIOB, GPIO1);  else gpio_set(GPIOB, GPIO1);}       //UPR 078 микширование не задействованы
+void setUpr2(bool set) { if(set) gpio_clear(GPIOB, GPIO2);  else gpio_set(GPIOB, GPIO2);}       //UPR ST  микширование не задействованы
 
-void setFan(bool set) { if(set) gpio_set(GPIOB, GPIO9);  else gpio_clear(GPIOB, GPIO9);}        //Вентилятор
+void setFan(bool set) { if(set) gpio_clear(GPIOB, GPIO9);  else gpio_set(GPIOB, GPIO9);}        //Вентилятор
 //#if (defined useI2C1 && defined  useDisplay)//i2c2 диагностический экранчик на i2c1
-void setLedShortOut(bool set) { if(set) gpio_set(GPIOB, GPIO10);  else gpio_clear(GPIOB, GPIO10);}    //КЗ ->
-void setLedBreakOut(bool set) { if(set) gpio_set(GPIOB, GPIO11);  else gpio_clear(GPIOB, GPIO11);}    //Обрыв ->
+void setLedShortOut(bool set) { if(set) gpio_clear(GPIOB, GPIO10);  else gpio_set(GPIOB, GPIO10);}    //КЗ ->
+void setLedBreakOut(bool set) { if(set) gpio_clear(GPIOB, GPIO11);  else gpio_set(GPIOB, GPIO11);}    //Обрыв ->
 //#endif
 
-void setLedOvercutOut(bool set) { if(set) gpio_set(GPIOB, GPIO12);  else gpio_clear(GPIOB, GPIO12);}  //Перегрузка ->
-void setLedOverheatOut(bool set) { if(set) gpio_set(GPIOB, GPIO13);  else gpio_clear(GPIOB, GPIO13);} //Перегрев ->
-void setErrorRele(bool set) { if(set) gpio_set(GPIOB, GPIO14);  else gpio_clear(GPIOB, GPIO14);}      //LED Авария + Авария ->
-void setReleLine1(bool set) { if(set) gpio_set(GPIOB, GPIO15);  else gpio_clear(GPIOB, GPIO15);}      //LED Линия1 + Вых линии 1 ->
-void setReleLine2(bool set) { if(set) gpio_set(GPIOC, GPIO6);  else gpio_clear(GPIOC, GPIO6);}        //LED Линия2
-void setReleLine3(bool set) { if(set) gpio_set(GPIOC, GPIO7);  else gpio_clear(GPIOC, GPIO7);}        //LED Линия3
-void setReleLine4(bool set) { if(set) gpio_set(GPIOC, GPIO8);  else gpio_clear(GPIOC, GPIO8);}        //LED Линия4
+void setLedOvercutOut(bool set) { if(set) gpio_clear(GPIOB, GPIO12);  else gpio_set(GPIOB, GPIO12);}  //Перегрузка ->
+void setLedOverheatOut(bool set) { if(set) gpio_clear(GPIOB, GPIO13);  else gpio_set(GPIOB, GPIO13);} //Перегрев ->
+void setErrorRele(bool set) { if(set) gpio_clear(GPIOB, GPIO14);  else gpio_set(GPIOB, GPIO14);}      //LED Авария + Авария ->
+void setReleLine1(bool set) { if(set) gpio_clear(GPIOB, GPIO15);  else gpio_set(GPIOB, GPIO15);}      //LED Линия1 + Вых линии 1 ->
+void setReleLine2(bool set) { if(set) gpio_clear(GPIOC, GPIO6);  else gpio_set(GPIOC, GPIO6);}        //LED Линия2
+void setReleLine3(bool set) { if(set) gpio_clear(GPIOC, GPIO7);  else gpio_set(GPIOC, GPIO7);}        //LED Линия3
+void setReleLine4(bool set) { if(set) gpio_clear(GPIOC, GPIO8);  else gpio_set(GPIOC, GPIO8);}        //LED Линия4
 
-void setRele24V(bool set) { if(set) gpio_set(GPIOC, GPIO9);  else gpio_clear(GPIOC, GPIO9);}          //LED Реле + Выход Реле ->
+void setRele24V(bool set) { if(set) gpio_clear(GPIOC, GPIO9);  else gpio_set(GPIOC, GPIO9);}          //LED Реле + Выход Реле ->
 
 //Свободные пины
-void setTestLed1(bool set) { if(set) gpio_set(GPIOC, GPIO15);  else gpio_clear(GPIOC, GPIO15);}
-void setTestLed2(bool set) { if(set) gpio_set(GPIOC, GPIO14);  else gpio_clear(GPIOC, GPIO14);}
-void setTestLed3(bool set) { if(set) gpio_set(GPIOC, GPIO5);  else gpio_clear(GPIOC, GPIO5);}
+void setTestLed1(bool set) { if(set) gpio_clear(GPIOC, GPIO15);  else gpio_set(GPIOC, GPIO15);}
+void setTestLed2(bool set) { if(set) gpio_clear(GPIOC, GPIO14);  else gpio_set(GPIOC, GPIO14);}
+void setTestLed3(bool set) { if(set) gpio_clear(GPIOC, GPIO5);  else gpio_set(GPIOC, GPIO5);}
 
 
--- 5_Amplifier_UM_300/ZS_2_STM32F103R8T6_no_rtos/src/set_output.c
+++ 5_Amplifier_UM_300/ZS_2_STM32F103R8T6_no_rtos/src/set_output.c
@@ -66,41 +66,41 @@ void setOutputs(void *args)
 
 //Нужно реализовать выходные сигналы
 
-        setErrorRele(1);
-        setReleLine1(1);
-        setReleLine2(1);
-        setReleLine3(1);
-        setReleLine4(1);
-        setReleTr1(1);
-        setReleTr2(1);
-        setReleTr3(1);
-        setReleTr4(1);
-        setReadyLed(1);
-        setLedOvercutOut(1);
-        setLedOverheatOut(1);
-        setLedShortOut(1);
-        setLedBreakOut(1);
-        setFan(0);
+//        setErrorRele(1);
+//        setReleLine1(1);
+//        setReleLine2(1);
+//        setReleLine3(1);
+//        setReleLine4(1);
+//        setReleTr1(1);
+//        setReleTr2(1);
+//        setReleTr3(1);
+//        setReleTr4(1);
+//        setReadyLed(1);
+//        setLedOvercutOut(1);
+//        setLedOverheatOut(1);
+//        setLedShortOut(1);
+//        setLedBreakOut(1);
+//        setRele24V(1);
+//        setFan(1);
+//        vTaskDelay(pdMS_TO_TICKS(2000));
 
-        vTaskDelay(pdMS_TO_TICKS(2000));
-
-        setErrorRele(0);
-        setReleLine1(0);
-        setReleLine2(0);
-        setReleLine3(0);
-        setReleLine4(0);
-        setReleTr1(0);
-        setReleTr2(0);
-        setReleTr3(0);
-        setReleTr4(0);
-        setReadyLed(0);
-        setLedOvercutOut(0);
-        setLedOverheatOut(0);
-        setLedShortOut(0);
-        setLedBreakOut(0);
-        setFan(0);
-
-        vTaskDelay(pdMS_TO_TICKS(2000));
+//        setErrorRele(0);
+//        setReleLine1(0);
+//        setReleLine2(0);
+//        setReleLine3(0);
+//        setReleLine4(0);
+//        setReleTr1(0);
+//        setReleTr2(0);
+//        setReleTr3(0);
+//        setReleTr4(0);
+//        setReadyLed(0);
+//        setLedOvercutOut(0);
+//        setLedOverheatOut(0);
+//        setLedShortOut(0);
+//        setLedBreakOut(0);
+//        setRele24V(0);
+//        setFan(0);
+//        vTaskDelay(pdMS_TO_TICKS(2000));
 
 
     }
