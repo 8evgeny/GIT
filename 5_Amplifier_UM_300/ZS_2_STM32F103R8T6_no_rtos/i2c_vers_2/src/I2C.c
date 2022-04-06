be1cb21e856b1d902d9eeb6b61b984c0b6e2a51a
Evgeny Parubets <8evgeny@gmail.com>
2022-04-06 11:40:48 +0300

    Начал программый i2c

--- 5_Amplifier_UM_300/ZS_2_STM32F103R8T6_no_rtos/i2c_vers_2/src/I2C.c
+++ 5_Amplifier_UM_300/ZS_2_STM32F103R8T6_no_rtos/i2c_vers_2/src/I2C.c
@@ -3,50 +3,6 @@
 
 Digital_POT_I2C_Def poti2c;
 
-void init_I2C1(void)
-{
-    rcc_periph_clock_enable(RCC_GPIOB);
-    rcc_periph_clock_enable(RCC_I2C1);
-    rcc_periph_clock_enable(RCC_AFIO);
-
-    gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
-              GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN,
-              GPIO_I2C1_SCL | GPIO_I2C1_SDA);
-
-    /* Disable the I2C before changing any configuration. */
-    i2c_peripheral_disable(I2C1);
-
-    /* APB1 is running at 36MHz. */
-    i2c_set_clock_frequency(I2C1, 36);
-
-    /* 400KHz - I2C Fast Mode */
-    i2c_set_fast_mode(I2C1);
-//    i2c_set_standard_mode(I2C1);
-
-    /*
-     * fclock for I2C is 36MHz APB2 -> cycle time 28ns, low time at 400kHz
-     * incl trise -> Thigh = 1600ns; CCR = tlow/tcycle = 0x1C,9;
-     * Datasheet suggests 0x1e.
-     */
-    i2c_set_ccr(I2C1, 0x1e);
-
-    /*
-     * fclock for I2C is 36MHz -> cycle time 28ns, rise time for
-     * 400kHz => 300ns and 100kHz => 1000ns; 300ns/28ns = 10;
-     * Incremented by 1 -> 11.
-     */
-    i2c_set_trise(I2C1, 0x0b);
-
-    /*
-     * This is our slave address - needed only if we want to receive from
-     * other masters.
-     */
-    i2c_set_own_7bit_slave_address(I2C1, 0x11);
-
-    /* If everything is configured -> enable the peripheral. */
-    i2c_peripheral_enable(I2C1);
-}
-
 void init_I2C2(void)
 {
     rcc_periph_clock_enable(RCC_GPIOB);
@@ -146,7 +102,6 @@ bool I2C_GetFlagStatus_(uint32_t i2c, uint32_t I2C_FLAG)
   return  bitstatus;
 }
 
-
 void I2C_StartTransmission(uint32_t i2c, uint8_t transmissionDirection,  uint8_t slaveAddress)
 {
       while((I2C_SR2(i2c) & I2C_SR2_BUSY) != 0x00000000){};
@@ -206,7 +161,6 @@ void I2C_WriteData(uint32_t i2c, uint8_t data)
           );
 }
 
-
 //uint8_t I2C_ReadData(I2C_TypeDef* I2Cx)
 uint8_t I2C_ReadData(uint32_t  i2c)
 {
@@ -223,6 +177,52 @@ uint8_t I2C_ReadData(uint32_t  i2c)
     return data;
 }
 
+
+
+
+void init_I2C1(void)
+{
+    rcc_periph_clock_enable(RCC_GPIOB);
+    rcc_periph_clock_enable(RCC_I2C1);
+    rcc_periph_clock_enable(RCC_AFIO);
+
+    gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+              GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN,
+              GPIO_I2C1_SCL | GPIO_I2C1_SDA);
+
+    /* Disable the I2C before changing any configuration. */
+    i2c_peripheral_disable(I2C1);
+
+    /* APB1 is running at 36MHz. */
+    i2c_set_clock_frequency(I2C1, 36);
+
+    /* 400KHz - I2C Fast Mode */
+    i2c_set_fast_mode(I2C1);
+//    i2c_set_standard_mode(I2C1);
+
+    /*
+     * fclock for I2C is 36MHz APB2 -> cycle time 28ns, low time at 400kHz
+     * incl trise -> Thigh = 1600ns; CCR = tlow/tcycle = 0x1C,9;
+     * Datasheet suggests 0x1e.
+     */
+    i2c_set_ccr(I2C1, 0x1e);
+
+    /*
+     * fclock for I2C is 36MHz -> cycle time 28ns, rise time for
+     * 400kHz => 300ns and 100kHz => 1000ns; 300ns/28ns = 10;
+     * Incremented by 1 -> 11.
+     */
+    i2c_set_trise(I2C1, 0x0b);
+
+    /*
+     * This is our slave address - needed only if we want to receive from
+     * other masters.
+     */
+    i2c_set_own_7bit_slave_address(I2C1, 0x11);
+
+    /* If everything is configured -> enable the peripheral. */
+    i2c_peripheral_enable(I2C1);
+}
 
 void I2C_POD_StartTransmission(uint32_t i2c, uint8_t transmissionDirection,  uint8_t slaveAddress)
 {
@@ -288,7 +288,95 @@ vTaskDelay(pdMS_TO_TICKS(1));
     }
 }
 
+void send_to_POT(uint8_t data)
+{
+    //AR - только запись
+    //57 - чтение
+    //56 - запись
+    //56 02 80 выбор WCR
+    //56 02 00 выбор DR
+    //The WCR is a volatile register and is written with the contents of the nonvolatile Data Register (DR) on power-up.
 
+    //WCR WRITE OPERATION                 start 56 02 80 stop start 56 00 data stop
+    //WCR INCREMENT/DECREMENT OPERATION - start 56 02 80 stop start 5E 00 ??
+    //WCR READ OPERATION                  start 56 02 80 stop start 56 00 start 57 data stop
+    //The WCR is also written during a write to DR
+    //DR WRITE OPERATION                  start 56 02 00 stop start 56 00 data stop
+    //DR READ OPERATION                   start 56 02 00 stop start 56 00 start 57 data stop
+
+    //start 56 02 00 stop start 56 00 data stop
+
+    //void i2c_send_start(uint32_t i2c)
+    //{
+    //	I2C_CR1(i2c) |= I2C_CR1_START; !!!!!!!!!!!!!!!!!!!!!!!!!!! тут все
+    //}
+
+//DR WRITE OPERATION  start 56 02 00 stop start 56 00 data stop
+    I2C_POD_StartTransmission(I2C1, I2C_Direction_Transmitter, 0x56);
+
+    i2c_send_data(I2C1,0x02);
+
+vTaskDelay(pdMS_TO_TICKS(1));
+
+//    while( // Выход из цикла когда все флаги упадут в 0
+//          ((I2C_SR1(I2C1) & I2C_SR1_TxE) == 0x00000000)  ||  //TXE
+//          ((I2C_SR1(I2C1) & I2C_SR1_BTF) == 0x00000000)  ||  //BTF
+//          ((I2C_SR2(I2C1) & I2C_SR2_BUSY) == 0x00000000) ||  //BUSY
+//          ((I2C_SR2(I2C1) & I2C_SR2_MSL) == 0x00000000)  ||  //MSL
+//          ((I2C_SR2(I2C1) & I2C_SR2_TRA) == 0x00000000)      //TRA
+//          );
+
+    i2c_send_data(I2C1,0x00);
+
+vTaskDelay(pdMS_TO_TICKS(1));
+
+//    while( // Выход из цикла когда все флаги упадут в 0
+//          ((I2C_SR1(I2C1) & I2C_SR1_TxE) == 0x00000000)  ||  //TXE
+//          ((I2C_SR1(I2C1) & I2C_SR1_BTF) == 0x00000000)  ||  //BTF
+//          ((I2C_SR2(I2C1) & I2C_SR2_BUSY) == 0x00000000) ||  //BUSY
+//          ((I2C_SR2(I2C1) & I2C_SR2_MSL) == 0x00000000)  ||  //MSL
+//          ((I2C_SR2(I2C1) & I2C_SR2_TRA) == 0x00000000)      //TRA
+//          );
+
+i2c_send_stop(I2C1);
+
+vTaskDelay(pdMS_TO_TICKS(1));
+
+I2C_POD_StartTransmission(I2C1, I2C_Direction_Transmitter, 0x56);
+
+i2c_send_data(I2C1,0x00);
+
+vTaskDelay(pdMS_TO_TICKS(1));
+
+//    while( // Выход из цикла когда все флаги упадут в 0
+//          ((I2C_SR1(I2C1) & I2C_SR1_TxE) == 0x00000000)  ||  //TXE
+//          ((I2C_SR1(I2C1) & I2C_SR1_BTF) == 0x00000000)  ||  //BTF
+//          ((I2C_SR2(I2C1) & I2C_SR2_BUSY) == 0x00000000) ||  //BUSY
+//          ((I2C_SR2(I2C1) & I2C_SR2_MSL) == 0x00000000)  ||  //MSL
+//          ((I2C_SR2(I2C1) & I2C_SR2_TRA) == 0x00000000)      //TRA
+//          );
+
+i2c_send_data(I2C1,data);
+
+vTaskDelay(pdMS_TO_TICKS(1));
+
+//    while( // Выход из цикла когда все флаги упадут в 0
+//          ((I2C_SR1(I2C1) & I2C_SR1_TxE) == 0x00000000)  ||  //TXE
+//          ((I2C_SR1(I2C1) & I2C_SR1_BTF) == 0x00000000)  ||  //BTF
+//          ((I2C_SR2(I2C1) & I2C_SR2_BUSY) == 0x00000000) ||  //BUSY
+//          ((I2C_SR2(I2C1) & I2C_SR2_MSL) == 0x00000000)  ||  //MSL
+//          ((I2C_SR2(I2C1) & I2C_SR2_TRA) == 0x00000000)      //TRA
+//          );
+
+    i2c_send_stop(I2C1);
+
+vTaskDelay(pdMS_TO_TICKS(1));
+
+}
+
+
+
+//Какой-то пример
 
 //#define SCL     TRISB4   // I2C bus
 //#define SDA     TRISB1   //
@@ -413,90 +501,3 @@ vTaskDelay(pdMS_TO_TICKS(1));
 //    rangelow = i2c_rx (0);       // get low byte of the range - note we don't acknowledge the last byte.
 //    i2c_stop();                 // send stop sequence
 //}
-
-
-void send_to_POT(uint8_t data)
-{
-    //AR - только запись
-    //57 - чтение
-    //56 - запись
-    //56 02 80 выбор WCR
-    //56 02 00 выбор DR
-    //The WCR is a volatile register and is written with the contents of the nonvolatile Data Register (DR) on power-up.
-
-    //WCR WRITE OPERATION                 start 56 02 80 stop start 56 00 data stop
-    //WCR INCREMENT/DECREMENT OPERATION - start 56 02 80 stop start 5E 00 ??
-    //WCR READ OPERATION                  start 56 02 80 stop start 56 00 start 57 data stop
-    //The WCR is also written during a write to DR
-    //DR WRITE OPERATION                  start 56 02 00 stop start 56 00 data stop
-    //DR READ OPERATION                   start 56 02 00 stop start 56 00 start 57 data stop
-
-    //start 56 02 00 stop start 56 00 data stop
-
-    //void i2c_send_start(uint32_t i2c)
-    //{
-    //	I2C_CR1(i2c) |= I2C_CR1_START; !!!!!!!!!!!!!!!!!!!!!!!!!!! тут все
-    //}
-
-//DR WRITE OPERATION  start 56 02 00 stop start 56 00 data stop
-    I2C_POD_StartTransmission(I2C1, I2C_Direction_Transmitter, 0x56);
-
-    i2c_send_data(I2C1,0x02);
-
-vTaskDelay(pdMS_TO_TICKS(1));
-
-//    while( // Выход из цикла когда все флаги упадут в 0
-//          ((I2C_SR1(I2C1) & I2C_SR1_TxE) == 0x00000000)  ||  //TXE
-//          ((I2C_SR1(I2C1) & I2C_SR1_BTF) == 0x00000000)  ||  //BTF
-//          ((I2C_SR2(I2C1) & I2C_SR2_BUSY) == 0x00000000) ||  //BUSY
-//          ((I2C_SR2(I2C1) & I2C_SR2_MSL) == 0x00000000)  ||  //MSL
-//          ((I2C_SR2(I2C1) & I2C_SR2_TRA) == 0x00000000)      //TRA
-//          );
-
-    i2c_send_data(I2C1,0x00);
-
-vTaskDelay(pdMS_TO_TICKS(1));
-
-//    while( // Выход из цикла когда все флаги упадут в 0
-//          ((I2C_SR1(I2C1) & I2C_SR1_TxE) == 0x00000000)  ||  //TXE
-//          ((I2C_SR1(I2C1) & I2C_SR1_BTF) == 0x00000000)  ||  //BTF
-//          ((I2C_SR2(I2C1) & I2C_SR2_BUSY) == 0x00000000) ||  //BUSY
-//          ((I2C_SR2(I2C1) & I2C_SR2_MSL) == 0x00000000)  ||  //MSL
-//          ((I2C_SR2(I2C1) & I2C_SR2_TRA) == 0x00000000)      //TRA
-//          );
-
-i2c_send_stop(I2C1);
-
-vTaskDelay(pdMS_TO_TICKS(1));
-
-I2C_POD_StartTransmission(I2C1, I2C_Direction_Transmitter, 0x56);
-
-i2c_send_data(I2C1,0x00);
-
-vTaskDelay(pdMS_TO_TICKS(1));
-
-//    while( // Выход из цикла когда все флаги упадут в 0
-//          ((I2C_SR1(I2C1) & I2C_SR1_TxE) == 0x00000000)  ||  //TXE
-//          ((I2C_SR1(I2C1) & I2C_SR1_BTF) == 0x00000000)  ||  //BTF
-//          ((I2C_SR2(I2C1) & I2C_SR2_BUSY) == 0x00000000) ||  //BUSY
-//          ((I2C_SR2(I2C1) & I2C_SR2_MSL) == 0x00000000)  ||  //MSL
-//          ((I2C_SR2(I2C1) & I2C_SR2_TRA) == 0x00000000)      //TRA
-//          );
-
-i2c_send_data(I2C1,data);
-
-vTaskDelay(pdMS_TO_TICKS(1));
-
-//    while( // Выход из цикла когда все флаги упадут в 0
-//          ((I2C_SR1(I2C1) & I2C_SR1_TxE) == 0x00000000)  ||  //TXE
-//          ((I2C_SR1(I2C1) & I2C_SR1_BTF) == 0x00000000)  ||  //BTF
-//          ((I2C_SR2(I2C1) & I2C_SR2_BUSY) == 0x00000000) ||  //BUSY
-//          ((I2C_SR2(I2C1) & I2C_SR2_MSL) == 0x00000000)  ||  //MSL
-//          ((I2C_SR2(I2C1) & I2C_SR2_TRA) == 0x00000000)      //TRA
-//          );
-
-    i2c_send_stop(I2C1);
-
-vTaskDelay(pdMS_TO_TICKS(1));
-
-}
