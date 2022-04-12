dcce75b3e35314a04ea1f3656eb260ab966657f2
Evgeny Parubets <8evgeny@gmail.com>
2022-04-12 15:24:01 +0300

    2

--- 5_Amplifier_UM_300/ZS_2_STM32F103R8T6_no_rtos/inc/main.h
+++ 5_Amplifier_UM_300/ZS_2_STM32F103R8T6_no_rtos/inc/main.h
@@ -5,9 +5,14 @@
 #ifdef uselibopencm3
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/exti.h>
+#include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/usart.h>
 #include <libopencm3/stm32/i2c.h>
 #endif
+
+
 
 #include <errno.h>
 #include <stdio.h>
