4d3b7eeb1c98f16e77ac07b72289237372f87f66
Evgeny Parubets <8evgeny@gmail.com>
2022-04-21 16:07:00 +0300

    RS232 без 2 функций HAL_UART_MspInit  readFromUartThread   и   Debug  +

--- 6_promwad2_step_by_step/SC2_FreeRTOS_CUBE_and_CMake/Core/Src/RS232/rs232.h
+++ 6_promwad2_step_by_step/SC2_FreeRTOS_CUBE_and_CMake/Core/Src/RS232/rs232.h
@@ -2,15 +2,19 @@
 #define RS232_H
 
 #include <cstdint>
-#include "stm32f7xx_hal.h"
-#include "stm32f7xx_hal_uart.h"
-#include "stm32f7xx_hal_dma.h"
-#include "stm32f7xx_it.h"
+#include "stm32h7xx_hal.h"
+#include "stm32h7xx_hal_uart.h"
+#include "stm32h7xx_hal_dma.h"
+#include "stm32h7xx_it.h"
 #include <array>
 #include "CircularBuffer.h"
-#include "sram.h"
 #include "cmsis_os.h"
-#include "json.h"
+
+
+//#include "sram.h"
+//#include "json.h"
+
+
 
 /*!
  * \brief The Console class for streaming debug information
