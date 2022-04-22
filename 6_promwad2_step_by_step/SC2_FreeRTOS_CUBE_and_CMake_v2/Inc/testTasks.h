b08e58ad3273a72b7ab2473c2edba37d205653cb
Evgeny Parubets <8evgeny@gmail.com>
2022-04-22 15:41:54 +0300

    Заменил HAL

---
+++ 6_promwad2_step_by_step/SC2_FreeRTOS_CUBE_and_CMake_v2/Inc/testTasks.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "main.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void simpleLedTest1_RTOS(void const *argument);
+void simpleLedTest2_RTOS(void const *argument);
+void simpleLedTest3_RTOS(void const *argument);
+void testLed1();
+void testLed2();
+void testLed3();
+
+#ifdef __cplusplus
+}
+#endif
+
+#include "rs232.h"
+void test_UART();
--- 6_promwad2_step_by_step/SC2_FreeRTOS_CUBE_and_CMake_v2/Src/SNTP/sntp_def.h
+++
@@ -1,4 +0,0 @@
-#ifndef SNTP_DEF_H
-#define SNTP_DEF_H
-
-#endif // SNTP_DEF_H
