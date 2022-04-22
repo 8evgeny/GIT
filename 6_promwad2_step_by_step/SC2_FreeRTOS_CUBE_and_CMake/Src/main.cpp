5a1372f3fa30d5414ac428fdf930d80f6e37674c
Evgeny Parubets <8evgeny@gmail.com>
2022-04-22 18:10:21 +0300

    +++

--- 6_promwad2_step_by_step/SC2_FreeRTOS_CUBE_and_CMake/Src/main.cpp
+++ 6_promwad2_step_by_step/SC2_FreeRTOS_CUBE_and_CMake/Src/main.cpp
@@ -601,7 +601,6 @@ static void MX_FMC_Init(void)
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
