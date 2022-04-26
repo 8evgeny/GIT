c4593b30cd4fa916eca50b9085684ed893a0241b
Evgeny Parubets <8evgeny@gmail.com>
2022-04-26 11:44:57 +0300

    ++

--- 6_promwad2_step_by_step/SC2_FreeRTOS_CUBE_and_CMake/Src/main.cpp
+++ 6_promwad2_step_by_step/SC2_FreeRTOS_CUBE_and_CMake/Src/main.cpp
@@ -38,13 +38,11 @@ static void MX_I2C1_Init(void);
 static void MX_I2C2_Init(void);
 static void MX_I2C3_Init(void);
 static void MX_SAI1_Init(void);
-static void MX_UART7_Init(void);
+//static void MX_UART7_Init(void);
 static void MX_TIM3_Init(void);
 static void MX_DMA_Init(void);
 static void MX_RNG_Init(void);
 void StartDefaultTask(void const * argument);
-
-
 
 I2C_HandleTypeDef hi2c1;
 I2C_HandleTypeDef hi2c2;
@@ -72,6 +70,40 @@ osThreadDef(StartWdtThread, StartWdtThread, osPriorityRealtime, 0, configMINIMAL
 osThreadDef(recvUdpThread, recvUdpThread, osPriorityHigh, 0, configMINIMAL_STACK_SIZE * 20);
 osThreadDef(audioInitThread, threadAudioInit, osPriorityHigh, 0, configMINIMAL_STACK_SIZE * 10);
 
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
+{
+    /* Run time stack overflow checking is performed if
+    configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+    function is called if a stack overflow is detected. */
+    while (1) {
+        RS232::getInstance().term << __FUNCTION__ << " " << __LINE__ << " " << "\n";
+    }
+}
+
+/* This default malloc failed hook does nothing and is declared as a weak symbol
+to allow the application writer to override this default by providing their own
+implementation in the application code. */
+void vApplicationMallocFailedHook(void)
+{
+
+    /* vApplicationMallocFailedHook() will only be called if
+    configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+    function that will get called if a call to pvPortMalloc() fails.
+    pvPortMalloc() is called internally by the kernel whenever a task, queue,
+    timer or semaphore is created.  It is also called by various parts of the
+    demo application.  If heap_1.c or heap_2.c are used, then the size of the
+    heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+    FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+    to query the size of free heap space that remains (although it does not
+    provide information on how the remaining heap might be fragmented). */
+    while (1) {
+        RS232::getInstance().term << __FUNCTION__ << " " << __LINE__ << " " << "\n";
+    }
+}
+
+#ifdef __cplusplus
+}
+#endif
 
 
 int main(void)
@@ -144,6 +176,11 @@ int main(void)
   }
   /* USER CODE END 3 */
 }
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void SystemClock_Config(void)
 {
@@ -538,7 +575,6 @@ static void MX_DMA_Init(void)
 
 }
 
-/* FMC initialization function */
 static void MX_FMC_Init(void)
 {
 
@@ -672,17 +708,6 @@ static void MX_GPIO_Init(void)
 
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/* USER CODE BEGIN Header_StartDefaultTask */
-/**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
-/* USER CODE END Header_StartDefaultTask */
 void StartDefaultTask(void const * argument)
 {
   /* init code for LWIP */
@@ -695,8 +720,6 @@ void StartDefaultTask(void const * argument)
   }
   /* USER CODE END 5 */
 }
-
-/* MPU Configuration */
 
 void MPU_Config(void)
 {
@@ -731,10 +754,6 @@ void MPU_Config(void)
 
 }
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
