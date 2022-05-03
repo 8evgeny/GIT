b8cc0bb7b2a72a45211184d64316a2f0a0c7e1dc
8evgeny <8evgeny@gmail.com>
2022-05-03 14:47:01 +0300

    ----

--- 6_promwad2_step_by_step/EXAMPLES_H753/BSP/BSP/SW4STM32/XIP_QSPI_InternalSRAM/Debug/Drivers/BSP/Components/ft5336_reg.d
+++
@@ -1,5 +0,0 @@
-Drivers/BSP/Components/ft5336_reg.o: \
- /home/evg/SOFT/Github/GIT/6_promwad2_step_by_step/EXAMPLES_H753/BSP/BSP/Drivers/BSP/Components/ft5336/ft5336_reg.c \
- /home/evg/SOFT/Github/GIT/6_promwad2_step_by_step/EXAMPLES_H753/BSP/BSP/Drivers/BSP/Components/ft5336/ft5336_reg.h
-
-/home/evg/SOFT/Github/GIT/6_promwad2_step_by_step/EXAMPLES_H753/BSP/BSP/Drivers/BSP/Components/ft5336/ft5336_reg.h:
--- 6_promwad2_step_by_step/EXAMPLES_H753/BSP/BSP/SW4STM32/XIP_QSPI_InternalSRAM/Debug/Example/SW4STM32/syscalls.d
+++
@@ -1,2 +0,0 @@
-Example/SW4STM32/syscalls.o: \
- /home/evg/SOFT/Github/GIT/6_promwad2_step_by_step/EXAMPLES_H753/BSP/BSP/SW4STM32/syscalls.c
