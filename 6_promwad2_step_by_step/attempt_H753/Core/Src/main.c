02d668c7529482587b1d32e13e6b9d5823ef4dca
Evgeny Parubets <8evgeny@gmail.com>
2022-05-19 16:50:48 +0300

    ++

--- 6_promwad2_step_by_step/attempt_H753/Core/Src/main.c
+++ 6_promwad2_step_by_step/attempt_H753/Core/Src/main.c
@@ -243,7 +243,7 @@ void MPU_Config(void)
   */
   MPU_InitStruct.Enable = MPU_REGION_ENABLE;
   MPU_InitStruct.Number = MPU_REGION_NUMBER0;
-  MPU_InitStruct.BaseAddress = 0x30040000;
+  MPU_InitStruct.BaseAddress = 0x30000000;
   MPU_InitStruct.Size = MPU_REGION_SIZE_32KB;
   MPU_InitStruct.SubRegionDisable = 0x0;
   MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
@@ -257,6 +257,7 @@ void MPU_Config(void)
   /** Initializes and configures the Region and the memory to be protected
   */
   MPU_InitStruct.Number = MPU_REGION_NUMBER1;
+  MPU_InitStruct.BaseAddress = 0x30040000;
   MPU_InitStruct.Size = MPU_REGION_SIZE_256B;
   MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
 
--- 6_promwad2_step_by_step/attempt_H753/Core/Src/stm32h7xx_it.c
+++ 6_promwad2_step_by_step/attempt_H753/Core/Src/stm32h7xx_it.c
@@ -57,7 +57,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-
+extern ETH_HandleTypeDef heth;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -181,6 +181,20 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32h7xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles Ethernet global interrupt.
+  */
+void ETH_IRQHandler(void)
+{
+  /* USER CODE BEGIN ETH_IRQn 0 */
+
+  /* USER CODE END ETH_IRQn 0 */
+  HAL_ETH_IRQHandler(&heth);
+  /* USER CODE BEGIN ETH_IRQn 1 */
+
+  /* USER CODE END ETH_IRQn 1 */
+}
 
 /* USER CODE BEGIN 1 */
 
