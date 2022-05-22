de74ce78d146c024774de799e9d4ef4196bd5cb2
8evgeny <8evgeny@gmail.com>
2022-05-22 09:07:09 +0300

    ++

--- 6_promwad2_step_by_step/attempt_H753/Core/Inc/stm32h7xx_it.h
+++ 6_promwad2_step_by_step/attempt_H753/Core/Inc/stm32h7xx_it.h
@@ -54,7 +54,7 @@ void UsageFault_Handler(void);
 void DebugMon_Handler(void);
 void DMA1_Stream0_IRQHandler(void);
 void DMA1_Stream1_IRQHandler(void);
-void DMA1_Stream4_IRQHandler(void);
+void DMA1_Stream2_IRQHandler(void);
 void TIM1_UP_IRQHandler(void);
 void ETH_IRQHandler(void);
 void UART7_IRQHandler(void);
--- 6_promwad2_step_by_step/attempt_H753/Core/Src/stm32h7xx_it.c
+++ 6_promwad2_step_by_step/attempt_H753/Core/Src/stm32h7xx_it.c
@@ -195,17 +195,17 @@ void DMA1_Stream1_IRQHandler(void)
 }
 
 /**
-  * @brief This function handles DMA1 stream4 global interrupt.
+  * @brief This function handles DMA1 stream2 global interrupt.
   */
-void DMA1_Stream4_IRQHandler(void)
+void DMA1_Stream2_IRQHandler(void)
 {
-  /* USER CODE BEGIN DMA1_Stream4_IRQn 0 */
+  /* USER CODE BEGIN DMA1_Stream2_IRQn 0 */
 
-  /* USER CODE END DMA1_Stream4_IRQn 0 */
+  /* USER CODE END DMA1_Stream2_IRQn 0 */
   HAL_DMA_IRQHandler(&hdma_uart7_tx);
-  /* USER CODE BEGIN DMA1_Stream4_IRQn 1 */
+  /* USER CODE BEGIN DMA1_Stream2_IRQn 1 */
 
-  /* USER CODE END DMA1_Stream4_IRQn 1 */
+  /* USER CODE END DMA1_Stream2_IRQn 1 */
 }
 
 /**
