def1676ef37951787cc912aa8f2b10a988ec9d66
Evgeny Parubets <8evgeny@gmail.com>
2022-05-25 16:47:43 +0300

    +++

--- 7_Camera/Project_Keil_Camera/UserCode/main.cpp
+++ 7_Camera/Project_Keil_Camera/UserCode/main.cpp
@@ -23,30 +23,33 @@ int main (void)
         PORT_ResetBits(MDR_PORTB, PORT_Pin_8);
         delay(0x3FFFF);
 
-        while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_RXFE) == SET);
-        PORT_SetBits(MDR_PORTB, PORT_Pin_9); //54 pin
+//        while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_RXFE) == SET);
+//        PORT_SetBits(MDR_PORTB, PORT_Pin_9); //54 pin
 
-//        for (int i = 0; i <7; ++i)
+        for (int i = 0; i <7; ++i)
+        {
+            while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_RXFE) == SET) //Если буфер FIFO запрещен, бит устанавливается в 1, когда буферный регистр приемника пуст
+            {
+                //Тут сделать проверку времени ожидания
+            }
+
+
+            receiveData.push_back (UART_ReceiveData(MDR_UART1));
+//            if((i == 0) && (receiveData.at(0) != 0xFF))
 //        {
-//            while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_RXFE) == SET) //Если буфер FIFO запрещен, бит устанавливается в 1, когда буферный регистр приемника пуст
-//            {
-//                //Тут сделать проверку времени ожидания
-//            }
-
-
-//            receiveData.push_back (UART_ReceiveData(MDR_UART1));
-////            if((i == 0) && (receiveData.at(0) != 0xFF))
-////        {
-////            receiveData.clear ();
-////            break;
-////        }
+//            receiveData.clear ();
+//            break;
 //        }
+        }
 
-//        if (receiveData.size () != 0) //Принята команда
-//        {
-//            PORT_SetBits(MDR_PORTB, PORT_Pin_9);
-////            out->setMoveOn(false);
-//        }
+        if (receiveData.size () != 0) //Принята команда
+        {
+            PORT_SetBits(MDR_PORTB, PORT_Pin_9);
+            delay(0x3FFFF);
+            PORT_ResetBits(MDR_PORTB, PORT_Pin_9);
+            delay(0x3FFFF);
+//            out->setMoveOn(false);
+        }
 
 
     }
