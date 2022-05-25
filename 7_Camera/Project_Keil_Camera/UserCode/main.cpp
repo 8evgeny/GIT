c3837bfdd7d172ce169fac8f74e8a5b48f8fe7a3
Evgeny Parubets <8evgeny@gmail.com>
2022-05-25 14:30:22 +0300

    +

--- 7_Camera/Project_Keil_Camera/UserCode/main.cpp
+++ 7_Camera/Project_Keil_Camera/UserCode/main.cpp
@@ -13,40 +13,37 @@ int main (void)
 
     std::vector<uint8_t> receiveData(7);
 
-
+    PORT_SetBits(MDR_PORTB, PORT_Pin_9);
     while (1)
     {
-        PORT_SetBits(MDR_PORTB, PORT_Pin_9);
-        PORT_SetBits(MDR_PORTB, PORT_Pin_10);
-        delay(0x3FFFF);
+//        PORT_SetBits(MDR_PORTB, PORT_Pin_9);
+//        PORT_SetBits(MDR_PORTB, PORT_Pin_10);
+//        delay(0x3FFFF);
 
-        PORT_ResetBits(MDR_PORTB, PORT_Pin_9);
-        PORT_ResetBits(MDR_PORTB, PORT_Pin_10);
-        delay(0x3FFFF);
-
-
-//        for (int i = 0; i <7; ++i)
-//        {
-//            while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_RXFE) == SET) //Если буфер FIFO запрещен, бит устанавливается в 1, когда буферный регистр приемника пуст
-//            {
-//                //Тут сделать проверку времени ожидания
-//            }
-//            receiveData.push_back (UART_ReceiveData(MDR_UART1));
-//            if((i == 0) && (receiveData.at(0) != 0xFF))
-//        {
-//            receiveData.clear ();
-//            break;
-//        }
-//        }
-
-//        if (receiveData.size () == 7) //Принята команда
-//        {
-//            PORT_ResetBits(MDR_PORTB, PORT_Pin_10);
-////            out->setMoveOn(false);
-//        }
+//        PORT_ResetBits(MDR_PORTB, PORT_Pin_9);
+//        PORT_ResetBits(MDR_PORTB, PORT_Pin_10);
+//        delay(0x3FFFF);
 
 
+        for (int i = 0; i <7; ++i)
+        {
+            while (UART_GetFlagStatus (MDR_UART1, UART_FLAG_RXFE) == SET) //Если буфер FIFO запрещен, бит устанавливается в 1, когда буферный регистр приемника пуст
+            {
+                //Тут сделать проверку времени ожидания
+            }
+            receiveData.push_back (UART_ReceiveData(MDR_UART1));
+            if((i == 0) && (receiveData.at(0) != 0xFF))
+        {
+            receiveData.clear ();
+            break;
+        }
+        }
 
+        if (receiveData.size () == 7) //Принята команда
+        {
+            PORT_ResetBits(MDR_PORTB, PORT_Pin_9);
+//            out->setMoveOn(false);
+        }
 
 
     }//end while (1)
