5a8b417892ca4bd3c8c706ebbb0dd417dd247fe8
Evgeny Parubets <8evgeny@gmail.com>
2022-05-26 17:19:33 +0300

    Продолжаю

--- 6_promwad2_step_by_step/attempt_H753/Core/Src/main.c
+++ 6_promwad2_step_by_step/attempt_H753/Core/Src/main.c
@@ -457,7 +457,7 @@ static void MX_DMA_Init(void)
   HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 5, 0);
   HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
   /* DMA1_Stream6_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Stream6_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(DMA1_Stream6_IRQn, 5, 0);
   HAL_NVIC_EnableIRQ(DMA1_Stream6_IRQn);
 
 }
