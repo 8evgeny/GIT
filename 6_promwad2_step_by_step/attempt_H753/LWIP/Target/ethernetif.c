353045aa6c794e7e86307620481b04f227151085
Evgeny Parubets <8evgeny@gmail.com>
2022-05-31 16:08:07 +0300

    ++

--- 6_promwad2_step_by_step/attempt_H753/LWIP/Target/ethernetif.c
+++ 6_promwad2_step_by_step/attempt_H753/LWIP/Target/ethernetif.c
@@ -373,8 +373,8 @@ static void low_level_init(struct netif *netif)
   /* create the task that handles the ETH_MAC */
 /* USER CODE BEGIN OS_THREAD_DEF_CREATE_CMSIS_RTOS_V1 */
 
-//  osThreadDef(EthIf, ethernetif_input, osPriorityRealtime, 0, INTERFACE_THREAD_STACK_SIZE);
-//  osThreadCreate (osThread(EthIf), netif);
+  osThreadDef(EthIf, ethernetif_input, osPriorityRealtime, 0, INTERFACE_THREAD_STACK_SIZE);
+  osThreadCreate (osThread(EthIf), netif);
 
 /* USER CODE END OS_THREAD_DEF_CREATE_CMSIS_RTOS_V1 */
 /* USER CODE BEGIN PHY_PRE_CONFIG */
