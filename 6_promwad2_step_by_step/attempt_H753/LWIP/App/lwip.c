8fb9ea527cf00e299e08504f1d7d84c73ac00c74
Evgeny Parubets <8evgeny@gmail.com>
2022-05-31 10:30:26 +0300

    ++

--- 6_promwad2_step_by_step/attempt_H753/LWIP/App/lwip.c
+++ 6_promwad2_step_by_step/attempt_H753/LWIP/App/lwip.c
@@ -60,7 +60,7 @@ void MX_LWIP_Init(void)
   /* IP addresses initialization */
   IP_ADDRESS[0] = 192;
   IP_ADDRESS[1] = 168;
-  IP_ADDRESS[2] = 1;
+  IP_ADDRESS[2] = 0;
   IP_ADDRESS[3] = 101;
   NETMASK_ADDRESS[0] = 255;
   NETMASK_ADDRESS[1] = 255;
