e4156fa166bd3b177f65026e8c7255b94a8f062d
Evgeny Parubets <8evgeny@gmail.com>
2022-06-01 13:01:27 +0300

    ХЗ не работает

--- 6_promwad2_step_by_step/attempt_H753/Core/Src/main.c
+++ 6_promwad2_step_by_step/attempt_H753/Core/Src/main.c
@@ -948,12 +948,31 @@ void StartDefaultTask(void const * argument)
     RS232_write_c("\rDP83848.No_Initialized\r\n", sizeof ("\rDP83848.No_Initialized\r\n"));
   }
 
+  const char* message = "Hello UDP message!\n\r";
+
+  osDelay(1000);
+
+  ip_addr_t PC_IPADDR;
+  IP_ADDR4(&PC_IPADDR, 192, 168, 0, 101);
+
+  struct udp_pcb* my_udp = udp_new();
+  udp_connect(my_udp, &PC_IPADDR, 55151);
+  struct pbuf* udp_buffer = NULL;
 //   udp_echoserver_init();
 
   /* Infinite loop */
   for(;;)
   {
 
+      osDelay(1000);
+        /* !! PBUF_RAM is critical for correct operation !! */
+        udp_buffer = pbuf_alloc(PBUF_TRANSPORT, strlen(message), PBUF_RAM);
+
+        if (udp_buffer != NULL) {
+          memcpy(udp_buffer->payload, message, strlen(message));
+          udp_send(my_udp, udp_buffer);
+          pbuf_free(udp_buffer);
+        }
       ethernetif_input(&gnetif);
       sys_check_timeouts();
       Ethernet_Link_Periodic_Handle(&gnetif);
