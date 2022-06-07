07df63e7150a280e5e791ea6b665bae330f5087b
Evgeny Parubets <8evgeny@gmail.com>
2022-06-07 16:45:14 +0300

    PullUP

--- 6_promwad2_step_by_step/attempt_H753/Core/Src/main.c
+++ 6_promwad2_step_by_step/attempt_H753/Core/Src/main.c
@@ -928,7 +928,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : PA8 */
   GPIO_InitStruct.Pin = GPIO_PIN_8;
   GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   GPIO_InitStruct.Alternate = GPIO_AF0_MCO;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
