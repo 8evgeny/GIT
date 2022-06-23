8699590d6475e599997cddfc0283d5490270b83d
8evgeny <8evgeny@gmail.com>
2022-06-23 22:54:22 +0300

    Осталось multiple definition 7штук

--- 6_promwad2_step_by_step/H753_new/Core/Src/main.cpp
+++ 6_promwad2_step_by_step/H753_new/Core/Src/main.cpp
@@ -271,7 +271,7 @@ int main(void)
 //            Debug::getInstance().dbg << __FUNCTION__ << " " << __LINE__ << " " << "\n";
 //        }
 
-        osThreadDef(recvUdpThread, recvUdpThread, osPriorityNormal, 0, configMINIMAL_STACK_SIZE * 20);
+//        osThreadDef(recvUdpThread, recvUdpThread, osPriorityNormal, 0, configMINIMAL_STACK_SIZE * 20);
 //        if ((UdpJsonExch::getInstance()->recvUdpThreadId = osThreadCreate(osThread(recvUdpThread), nullptr)) == nullptr)
 //        {
 //            Debug::getInstance().dbg << __FUNCTION__ << " " << __LINE__ << " " << "\n";
