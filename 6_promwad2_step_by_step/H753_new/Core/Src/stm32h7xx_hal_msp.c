e260bb40ddc761f51eb779001da9565efdbc8ae0
Evgeny Parubets <8evgeny@gmail.com>
2022-08-02 12:48:45 +0300

    ADD DMA

--- 6_promwad2_step_by_step/H753_new/Core/Src/stm32h7xx_hal_msp.c
+++ 6_promwad2_step_by_step/H753_new/Core/Src/stm32h7xx_hal_msp.c
@@ -31,6 +31,9 @@ extern DMA_HandleTypeDef hdma_i2c1_tx;
 extern DMA_HandleTypeDef hdma_i2c2_rx;
 extern DMA_HandleTypeDef hdma_i2c2_tx;
 
+extern DMA_HandleTypeDef hdma_i2c3_rx;
+extern DMA_HandleTypeDef hdma_i2c3_tx;
+
 extern DMA_HandleTypeDef hdma_uart7_rx;
 extern DMA_HandleTypeDef hdma_uart7_tx;
 
@@ -169,9 +172,9 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
 
     /* USER CODE END I2C1_MspInit 1 */
     }
+
     else if(hi2c->Instance==I2C2)
     {
-RS232Puts("hi2c->Instance==I2C2\r\n") ;
     /* USER CODE BEGIN I2C2_MspInit 0 */
 
     /* USER CODE END I2C2_MspInit 0 */
@@ -246,9 +249,9 @@ RS232Puts("hi2c->Instance==I2C2\r\n") ;
 
     /* USER CODE END I2C2_MspInit 1 */
     }
+
     else if(hi2c->Instance==I2C3)
     {
-RS232Puts("hi2c->Instance==I2C3\r\n") ;
         /** Initializes the peripherals clock
         */
           PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_I2C3;
@@ -280,9 +283,47 @@ RS232Puts("hi2c->Instance==I2C3\r\n") ;
 
           /* Peripheral clock enable */
           __HAL_RCC_I2C3_CLK_ENABLE();
+
+          hdma_i2c3_rx.Instance = DMA1_Stream0;
+          hdma_i2c3_rx.Init.Request = DMA_REQUEST_I2C3_RX;
+          hdma_i2c3_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+          hdma_i2c3_rx.Init.PeriphInc = DMA_PINC_DISABLE;
+          hdma_i2c3_rx.Init.MemInc = DMA_MINC_ENABLE;
+          hdma_i2c3_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+          hdma_i2c3_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+          hdma_i2c3_rx.Init.Mode = DMA_NORMAL;
+          hdma_i2c3_rx.Init.Priority = DMA_PRIORITY_LOW;
+          hdma_i2c3_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+          if (HAL_DMA_Init(&hdma_i2c3_rx) != HAL_OK)
+          {
+            Error_Handler();
+          }
+
+          __HAL_LINKDMA(hi2c,hdmarx,hdma_i2c3_rx);
+
+          /* I2C2_TX Init */
+          hdma_i2c3_tx.Instance = DMA1_Stream7;
+          hdma_i2c3_tx.Init.Request = DMA_REQUEST_I2C3_TX;
+          hdma_i2c3_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+          hdma_i2c3_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+          hdma_i2c3_tx.Init.MemInc = DMA_MINC_ENABLE;
+          hdma_i2c3_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+          hdma_i2c3_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+          hdma_i2c3_tx.Init.Mode = DMA_NORMAL;
+          hdma_i2c3_tx.Init.Priority = DMA_PRIORITY_LOW;
+          hdma_i2c3_tx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+          if (HAL_DMA_Init(&hdma_i2c3_tx) != HAL_OK)
+          {
+            Error_Handler();
+          }
+
+          __HAL_LINKDMA(hi2c,hdmatx,hdma_i2c3_tx);
+
           /* I2C3 interrupt Init */
           HAL_NVIC_SetPriority(I2C3_EV_IRQn, 5, 0);
           HAL_NVIC_EnableIRQ(I2C3_EV_IRQn);
+          HAL_NVIC_SetPriority(I2C3_ER_IRQn, 5, 0);
+          HAL_NVIC_EnableIRQ(I2C3_ER_IRQn);
     }
 }
 
@@ -319,9 +360,6 @@ void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
     }
     else if(hi2c->Instance==I2C2)
     {
-    /* USER CODE BEGIN I2C2_MspDeInit 0 */
-
-    /* USER CODE END I2C2_MspDeInit 0 */
       /* Peripheral clock disable */
       __HAL_RCC_I2C2_CLK_DISABLE();
 
@@ -340,15 +378,9 @@ void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
       /* I2C2 interrupt DeInit */
       HAL_NVIC_DisableIRQ(I2C2_EV_IRQn);
       HAL_NVIC_DisableIRQ(I2C2_ER_IRQn);
-    /* USER CODE BEGIN I2C2_MspDeInit 1 */
-
-    /* USER CODE END I2C2_MspDeInit 1 */
     }
     else if(hi2c->Instance==I2C3)
     {
-    /* USER CODE BEGIN I2C3_MspDeInit 0 */
-
-    /* USER CODE END I2C3_MspDeInit 0 */
       /* Peripheral clock disable */
       __HAL_RCC_I2C3_CLK_DISABLE();
 
@@ -360,11 +392,13 @@ void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
 
       HAL_GPIO_DeInit(GPIOC, GPIO_PIN_9);
 
+      /* I2C3 DMA DeInit */
+      HAL_DMA_DeInit(hi2c->hdmarx);
+      HAL_DMA_DeInit(hi2c->hdmatx);
+
       /* I2C3 interrupt DeInit */
       HAL_NVIC_DisableIRQ(I2C3_EV_IRQn);
-    /* USER CODE BEGIN I2C3_MspDeInit 1 */
-
-    /* USER CODE END I2C3_MspDeInit 1 */
+      HAL_NVIC_DisableIRQ(I2C3_ER_IRQn);
     }
 }
 
