904d679117fc4f43408720eaafff115f2a1b2b98
8evgeny <8evgeny@gmail.com>
2022-10-25 21:31:29 +0300

    Получаю уникальный 12 символьный (96 бит) идентификатор

--- 6_promwad2_step_by_step/H753_new/Core/Src/uid/uid.cpp
+++ 6_promwad2_step_by_step/H753_new/Core/Src/uid/uid.cpp
@@ -17,7 +17,7 @@
 
 #include "uid.h"
 #include "rs232.h"
-
+extern char uid[25];
 UID *UID::pInstance = nullptr;
 UIDDestroyer UID::destroyer;
 
@@ -40,13 +40,13 @@ UID &UID::getInstance()
     return *pInstance;
 }
 
-void UID::test()
+void UID::getUID()
 {
+//    char temp[100];
     uint32_t firstPart = UID::getInstance().getFirstPartOfUid();
     uint32_t secondPart = UID::getInstance().getSecondPartOfUid();
     uint32_t thirdPart = UID::getInstance().getThirdPartOfUid();
-
-    RS232::getInstance().term << "First part " << firstPart << "\n";
-    RS232::getInstance().term << "Second part " << secondPart << "\n";
-    RS232::getInstance().term << "Third part " << thirdPart << "\n";
+    sprintf(uid,"%08X%08X%08X",firstPart,secondPart,thirdPart);
+//    sprintf(temp,"UID - %s",uid);
+//    term2(temp)
 }
