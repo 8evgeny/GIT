837aa765a5b9e14a8f2de367f25630fd82533397
8evgeny <8evgeny@gmail.com>
2022-10-27 07:45:11 +0300

    Delay 250 - еще работает

--- 6_promwad2_step_by_step/H753_new/Core/Src/WDT/wdt.cpp
+++ 6_promwad2_step_by_step/H753_new/Core/Src/WDT/wdt.cpp
@@ -66,7 +66,7 @@ void WDT::Loop()
             term2(count)
         }
 
-        if (count < 1000)
+        if (count < 100)
         {
             refresh();
         }
@@ -85,7 +85,7 @@ WDT::WDT()
     wwdgtHandle = &hwwdg;
     running_ = true;
 
-    SetDelayTime(20);
+    SetDelayTime(250);
 
 }
 
