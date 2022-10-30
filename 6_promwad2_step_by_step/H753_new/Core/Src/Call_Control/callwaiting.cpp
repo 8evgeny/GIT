0b1b461009abff15dea4a77dc45d91235a90c289
8evgeny <8evgeny@gmail.com>
2022-10-30 07:07:54 +0300

    Fake Thread ON

--- 6_promwad2_step_by_step/H753_new/Core/Src/Call_Control/callwaiting.cpp
+++ 6_promwad2_step_by_step/H753_new/Core/Src/Call_Control/callwaiting.cpp
@@ -16,13 +16,13 @@ extern lfs_t lfs;
 extern lfs_file_t file;
 extern uint8_t boardType;
 
-//void fakeThread(void const *argument)
-//{
-//    (void)argument;
-//    while(1) {
+void fakeThread(void const *argument)
+{
+    (void)argument;
+    while(1) {
 
-//    }
-//}
+    }
+}
 
 CallWaiting::CallWaiting()
 {
@@ -105,11 +105,10 @@ term2(msg)
 
     case CallControl::Telephone:
     {
-            //Костыль
             if (context_->subjectKey.key == CallControl::Hash)
             {
-//                osThreadDef(fakeThread, fakeThread, osPriorityRealtime, 0, configMINIMAL_STACK_SIZE );
-//                osThreadCreate(osThread(fakeThread), nullptr);
+                osThreadDef(fakeThread, fakeThread, osPriorityRealtime, 0, configMINIMAL_STACK_SIZE );
+                osThreadCreate(osThread(fakeThread), nullptr);
 
 
 //                if (SAI::getInstance()->tone.status == DTMF::Status::START)
