f93716f9ecda39281915013e5f1cc6aee5b54b5e
Evgeny Parubets <8evgeny@gmail.com>
2023-04-21 18:18:55 +0300

    FIX WORK

---
+++ 13_web_interface/README.md
@@ -0,0 +1,3 @@
+web interface
+
+Исходный код в другом репозитории - STM32CUBEIDE  /home/evg/Stm32CubeIDE/STM32F407-DISCOVERY_W5500_INTRON
---
+++ 13_web_interface/SD_CARD/1.js
@@ -0,0 +1,6 @@
+let myButton = document.querySelector("#myButton"); 
+myButton.addEventListener("click", doSomething, false);
+function doSomething(e) 
+{ 
+	alert("Is this working?"); 
+} 
