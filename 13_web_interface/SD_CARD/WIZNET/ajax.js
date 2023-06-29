ebb45a902d91fc5b1d379886964e6593742ebc65
Evgeny Parubets <8evgeny@gmail.com>
2023-06-29 12:23:22 +0300

    При внесении js в код тест WIZNET не работает

--- 13_web_interface/SD_CARD/WIZNET/ajax.js
+++ 13_web_interface/SD_CARD/WIZNET/ajax.js
@@ -1,70 +1,70 @@
-    function AJAX(a,e)
+function AJAX(a,e)
+{
+    var c=d();
+    c.onreadystatechange=b;
+    function d()
     {
-        var c=d();
-        c.onreadystatechange=b;
-        function d()
+        if(window.XMLHttpRequest)
         {
-            if(window.XMLHttpRequest)
+            return new XMLHttpRequest()
+        }
+        else
+        {
+            if(window.ActiveXObject)
             {
-                return new XMLHttpRequest()
+                return new ActiveXObject(\Microsoft.XMLHTTP\)
             }
-            else
+        }
+    }
+    function b()
+    {
+        if(c.readyState==4)
+        {
+            if(c.status==200)
             {
-                if(window.ActiveXObject)
+                if(e)
                 {
-                    return new ActiveXObject(\Microsoft.XMLHTTP\)
+                    e(c.responseText)
                 }
             }
         }
-        function b()
-        {
-            if(c.readyState==4)
-            {
-                if(c.status==200)
-                {
-                    if(e)
-                    {
-                        e(c.responseText)
-                    }
-                }
-            }
-        }
-        this.doGet=function()
-        {
-            c.open(\GET\,a,true);
-            c.send(null)
-        };
-        this.doPost=function(f)
-        {
-            c.open(\POST\,a,true);
-            c.setRequestHeader(\Content-Type\,\application/x-www-form-urlencoded\);
-            c.setRequestHeader(\ISAJAX\,\yes\);
-            c.send(f)
-        }
     }
-    function $(a)
+    this.doGet=function()
     {
-        return document.getElementById(a)
-    }
-    function $$(a)
+        c.open(\GET\,a,true);
+        c.send(null)
+    };
+    this.doPost=function(f)
     {
-        return document.getElementsByName(a)
+        c.open(\POST\,a,true);
+        c.setRequestHeader(\Content-Type\,\application/x-www-form-urlencoded\);
+        c.setRequestHeader(\ISAJAX\,\yes\);
+        c.send(f)
     }
-    function $$_ie(a,c)
+}
+function $(a)
+{
+    return document.getElementById(a)
+}
+function $$(a)
+{
+    return document.getElementsByName(a)
+}
+function $$_ie(a,c)
+{
+    if(!a)
     {
-        if(!a)
-        {
-            a=\*\
-        }
-        var b=document.getElementsByTagName(a);
-        var e=[];
-        for(var d=0;d<b.length;d++)
-        {
-            att=b[d].getAttribute(\name\);
-            if(att==c)
-            {
-                e.push(b[d])
-            }
-        }
-        return e
+        a=\*\
     }
+    var b=document.getElementsByTagName(a);
+    var e=[];
+    for(var d=0;d<b.length;d++)
+    {
+        att=b[d].getAttribute(\name\);
+        if(att==c)
+        {
+            e.push(b[d])
+        }
+    }
+    return e
+}
