6dab23e805722b6363cf0d7fe8d083eb5370ad2c
8evgeny <8evgeny@gmail.com>
2023-10-02 18:21:36 +0300

    FIX

---
+++ 15_Документы_НИОКР/json_parser/main.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <QCoreApplication>
+#include <QFile>
+#include "rapidjson/document.h"
+#include "rapidjson/error/en.h"
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+using namespace rapidjson;
+using namespace std;
+using namespace chrono_literals;
+
+void parseJSON(QString & jsonData);
