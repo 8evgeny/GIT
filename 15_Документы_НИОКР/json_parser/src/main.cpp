a2c1e1e2e4d390b8e649dc8a99480cdfa600c075
8evgeny <8evgeny@gmail.com>
2023-10-06 12:29:49 +0300

    Путь - аргумент

--- 15_Документы_НИОКР/json_parser/src/main.cpp
+++ 15_Документы_НИОКР/json_parser/src/main.cpp
@@ -2,9 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-    const path archiv_path{ R"(/home/evg/SOFT/Github/GIT/15_Документы_НИОКР/temp_arсhive)" };
-//    const path archiv_path{ R"(/home/evg/Desktop/temp_arсhive)" };
-//    const path archiv_path{ R"(C:\Users\Professional\Documents\GIT1\15_Документы_НИОКР\temp_arсhive)" };
+    const path archiv_path{ argv[1] };
     const unordered_set<string> pe_extensions{ ".json" };
     cout << endl <<"Patch for json search: "<<archiv_path << endl;
     vector<string> vectorJsonFiles;
