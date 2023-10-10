f26d39c18db2f17ddc9a9e2d838ed0ba9592a176
8evgeny <8evgeny@gmail.com>
2023-10-10 10:56:15 +0300

    separator

--- 15_Документы_НИОКР/json_parser/src/jsonParser.cpp
+++ 15_Документы_НИОКР/json_parser/src/jsonParser.cpp
@@ -30,8 +30,22 @@ string createStringForQr (string oboznachenieIkodDokumenta,
                           )
 {
 //    ГРЛМ.741138.003 | изм.0 | К.001-2023 | 2023-05-29 | Лит.нет | CRC-32.pdf=1F349207 | CRC-32.zip\Contents=3C3DE8F8
+    string separator = "__";
+    string LiteraQr{"Лит."};
+    if (litera == "")
+        LiteraQr.append("нет");
+    else
+        LiteraQr.append(litera);
 
-    return oboznachenieIkodDokumenta+changeNumStr+changeNotificationNum+storageDataStr+litera+contromSummOrigin+contromSummParts;
+    string result = oboznachenieIkodDokumenta + separator +
+            "изм."+changeNumStr + separator +
+            changeNotificationNum + separator +
+            storageDataStr  + separator +
+            LiteraQr + separator+
+            "CRC-32.pdf="+contromSummOrigin + separator +
+            "CRC-32.zip\\Contents=" + contromSummParts;
+    cout<<result<<endl;
+    return result;
 }
 
 bool parseJSON(string & patchToFile){
@@ -178,6 +192,7 @@ QRegExp iulStr("^"
                         changeNum =  requisites["Номер изменения"].GetInt();
                         if(!number2dig.exactMatch(QString::number(changeNum))) return false;
                     cout << "\tНомер изменения: " << changeNum << "\n";
+                    changeNumStr = to_string(changeNum);
                 }else return false;
                 if (requisites.HasMember("Номер извещения об изменении")){
                     changeNotificationNum = requisites["Номер извещения об изменении"].GetString();
