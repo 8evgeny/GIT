1e99d173fc7caee6fd0dd310d51164cd030772d5
8evgeny <8evgeny@gmail.com>
2023-10-11 16:22:17 +0300

    таблица fix

--- 16_WEB_ARHIV/CONTENT/table.js
+++ 16_WEB_ARHIV/CONTENT/table.js
@@ -1,14 +1,14 @@
 
 function tableCreate() {
     const body = document.body,
-          // tbl = document.createElement('table2');
+          // tbl = document.createElement('table');
           tbl = document.getElementById('table');
-          
-    tbl.style.width = '1200px';
+  
+    tbl.style.width = '1800px';
     tbl.style.border = '1px solid black';
     for (let i = 0; i < 100; i++) {
       const tr = tbl.insertRow();
-      for (let j = 0; j < 7; j++) {
+      for (let j = 0; j < 8; j++) {
         // if (i === 1 && j === 0) {
         //   break;
         // } else 
@@ -16,21 +16,38 @@ function tableCreate() {
         {
           const td = tr.insertCell();
           td.style.border = '1px solid black';
+          td.style.fontWeight = 'bold';
+          td.style.textAlign="center"; 
+          td.style.fontStyle = "italic";
           switch(j){
-          case 0: td.appendChild(document.createTextNode(`1`));break;
-          case 1: td.appendChild(document.createTextNode(`2`));break;
-          case 2: td.appendChild(document.createTextNode(`3`));break;
-          case 3: td.appendChild(document.createTextNode(`4`));break;
-          case 4: td.appendChild(document.createTextNode(`5`));break;
-          case 5: td.appendChild(document.createTextNode(`6`));break;
-          case 6: td.appendChild(document.createTextNode(`7`));break;                   
+          case 1: td.appendChild(document.createTextNode(`Обозначение и код документа`));break;
+          case 2: td.appendChild(document.createTextNode(`Наименование изделия`));break;
+          case 3: td.appendChild(document.createTextNode(`Наименование документа`));break;
+          case 4: td.appendChild(document.createTextNode(`Номер изменения`));break;
+          case 5: td.appendChild(document.createTextNode(`Дата извещения об изменении`));break;
+          case 6: td.appendChild(document.createTextNode(`Значение контрольной суммы подлинника`));break;
+          case 7: td.appendChild(document.createTextNode(`Информационно-удостоверяющий лист`));break;    
+          case 0: td.appendChild(document.createTextNode(`Порядковый номер`));break;               
           }
         }
-        else
+        if (i!=0)
         {
-          const td = tr.insertCell();
-          td.appendChild(document.createTextNode(`Cell I${i}/J${j}`));
-          td.style.border = '1px solid black';
+          if (j==0)
+          {
+            const td = tr.insertCell();
+            td.appendChild(document.createTextNode(i));
+            td.style.border = '1px solid black';
+            td.style.textAlign="center";
+          }
+          else
+          {
+            const td = tr.insertCell();
+            td.appendChild(document.createTextNode(`Cell I${i}/J${j}`));
+            td.style.border = '1px solid black';
+            td.style.textAlign="center";
+          }
+
+
         }
       }
     }
