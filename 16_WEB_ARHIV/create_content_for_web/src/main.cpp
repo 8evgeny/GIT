a1d378087198c76d1ce0e8631f00a1b31df099ce
8evgeny <8evgeny@gmail.com>
2023-10-11 18:34:36 +0300

    FIX

---
+++ 16_WEB_ARHIV/create_content_for_web/src/main.cpp
@@ -0,0 +1,49 @@
+﻿#include "main.h"
+
+int main(int argc, char *argv[])
+{
+    const path archiv_path{ argv[1] };
+    const unordered_set<string> pe_extensions{ ".json" };
+    cout << endl <<"Patch for json search: "<<archiv_path << endl;
+    vector<string> vectorJsonFiles;
+    vector<string> errorJsonPatch;
+
+    auto iterator = recursive_directory_iterator{ archiv_path, directory_options::skip_permission_denied };
+    for(const auto& entry : iterator) {
+      try {
+        if(!entry.is_regular_file())
+          continue;
+        const auto& extension = entry.path().extension().string();
+        const auto is_pe = pe_extensions.find(extension) != pe_extensions.end();
+        if(!is_pe)
+          continue;
+        ifstream file{ entry.path() };
+        string patch = entry.path().string();
+        vectorJsonFiles.push_back(patch);
+      } catch(const exception& e) {
+        cerr << "Error reading " << entry.path().string() << ": " << e.what() << endl;
+      }
+    }
+    int error = 0;
+    if (vectorJsonFiles.size()>0)
+    {
+        int num = 0;
+        for (auto & patchJsonFile : vectorJsonFiles){
+            ++num;
+            cout << endl<< num << ": "<< patchJsonFile << endl;
+            if(!parseJSON(patchJsonFile)) //парсинг одного файла JSON
+            {
+                cout << "ОШИБКА JSON ФАЙЛА" << endl;
+                ++error;
+                errorJsonPatch.push_back(patchJsonFile);
+            }
+        }
+    }
+
+    cout<< endl<< "Всего разобрано json файлов: " << vectorJsonFiles.size()
+       << endl << "Ошибок разбора: " << error<< endl;
+    for (auto & patchJsonError : errorJsonPatch)
+        cout <<  patchJsonError << endl;
+
+
+}
