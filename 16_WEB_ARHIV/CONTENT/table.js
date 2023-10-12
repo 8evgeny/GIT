88244583c313318c16c36beb63627ba6f26c4892
8evgeny <8evgeny@gmail.com>
2023-10-12 15:32:12 +0300

    Число строк в таблице соответствует файлу numDoc

--- 16_WEB_ARHIV/CONTENT/table.js
+++ 16_WEB_ARHIV/CONTENT/table.js
@@ -6,81 +6,99 @@ function sleep(milliseconds)
     currentDate = Date.now();
   } while (currentDate - date < milliseconds);
 };
-function tableCreate() {
-    const body = document.body,
-//           tbl = document.createElement('table');
-    tbl = document.getElementById('table');
-    tbl.style.margin = 'auto';
-    tbl.style.width = '1800px';
-    tbl.style.border = '1px solid black';
-    for (let i = 0; i < 33; i++) {
-      const tr = tbl.insertRow();
-      for (let j = 0; j < 8; j++) {
-        // if (i === 1 && j === 0) {
-        //   break;
-        // } else 
-        if (i==0)
-        {
-          const td = tr.insertCell();
-          td.style.border = '1px solid black';
-          td.style.fontWeight = 'bold';
-          td.style.textAlign="center"; 
-          td.style.fontStyle = "italic";
-          switch(j){
-          case 1: td.appendChild(document.createTextNode(`Обозначение и код документа`));break;
-          case 2: td.appendChild(document.createTextNode(`Наименование изделия`));break;
-          case 3: td.appendChild(document.createTextNode(`Наименование документа`));break;
-          case 4: td.appendChild(document.createTextNode(`Номер изменения`));break;
-          case 5: td.appendChild(document.createTextNode(`Дата извещения об изменении`));break;
-          case 6: td.appendChild(document.createTextNode(`Значение контрольной суммы подлинника`));break;
-          case 7: td.appendChild(document.createTextNode(`Информационно-удостоверяющий лист`));break;    
-          case 0: td.appendChild(document.createTextNode(`Порядковый номер`));break;               
-          }
-        }
-        if (i!=0)
-        {
-          if (j==0)
-          {
-            const td = tr.insertCell();
-            td.appendChild(document.createTextNode(i));
-            td.style.border = '1px solid black';
-            td.style.textAlign="center";
-          }
-          else
-          {
-            const td = tr.insertCell();
-            td.appendChild(document.createTextNode(`Cell I${i}/J${j}`));
-            td.style.border = '1px solid black';
-            td.style.textAlign="center";
-          }
+
+// var htReq = ('v' == '\v') ? new ActiveXObject ('Microsoft.XMLHTTP') : new XMLHttpRequest ();
+// function sendReq (filePath)
+// {
+// htReq.open ('get', filePath);
+// htReq.onreadystatechange = getReq;
+// htReq.send (null);
+// htReq.responseText.toString()
+// }
+// var numberDoc;
+//  function getReq ()
+// {
+// if (htReq.readyState == 4) 
+// {
+//   numberDoc = htReq.responseText.toString();
+//   console.log (numberDoc); 
+// }
+// }
+// sendReq ('content_for_web/numDoc');
 
 
-        }
-      }
-    }
-    body.appendChild(tbl);
-  }
+
 
   var htReq = ('v' == '\v') ? new ActiveXObject ('Microsoft.XMLHTTP') : new XMLHttpRequest ();
   function sendReq (filePath)
   {
-  htReq.open ('get', filePath);
-  htReq.onreadystatechange = getReq;
-  htReq.send (null);
-
+    htReq.open ('get', filePath);
+    htReq.onreadystatechange = getReq;
+    htReq.send (null);
+    htReq.responseText.toString()
   }
-   function getReq ()
+  function getReq ()
   {
-  if (htReq.readyState == 4) 
-  {
-    var jsonText = htReq.responseText;
-    console.log (jsonText); 
-  }
-  }
+    if (htReq.readyState == 4) 
+    {
+      let numberDoc = htReq.responseText.toString();
+      function tableCreate() {
+        const body = document.body,
+        tbl = document.getElementById('table');
+        tbl.style.margin = 'auto';
+        tbl.style.width = '1800px';
+        tbl.style.border = '1px solid black';
+        for (let i = 0; i <= numberDoc ; i++) {
+          const tr = tbl.insertRow();
+          for (let j = 0; j < 8; j++) {
+            if (i==0)
+            {
+              const td = tr.insertCell();
+              td.style.border = '1px solid black';
+              td.style.fontWeight = 'bold';
+              td.style.textAlign="center"; 
+              td.style.fontStyle = "italic";
+              switch(j){
+              case 1: td.appendChild(document.createTextNode(`Обозначение и код документа`));break;
+              case 2: td.appendChild(document.createTextNode(`Наименование изделия`));break;
+              case 3: td.appendChild(document.createTextNode(`Наименование документа`));break;
+              case 4: td.appendChild(document.createTextNode(`Номер изменения`));break;
+              case 5: td.appendChild(document.createTextNode(`Дата извещения об изменении`));break;
+              case 6: td.appendChild(document.createTextNode(`Значение контрольной суммы подлинника`));break;
+              case 7: td.appendChild(document.createTextNode(`Информационно-удостоверяющий лист`));break;    
+              case 0: td.appendChild(document.createTextNode(`Порядковый номер`));break;               
+              }
+            }
+            if (i!=0)
+            {
+              if (j==0)
+              {
+                const td = tr.insertCell();
+                td.appendChild(document.createTextNode(i));
+                td.style.border = '1px solid black';
+                td.style.textAlign="center";
+              }
+              else
+              {
+                const td = tr.insertCell();
+                td.appendChild(document.createTextNode(`Cell I${i}/J${j}`));
+                td.style.border = '1px solid black';
+                td.style.textAlign="center";
+              }
+    
+    
+            }
+          }
+        }
+        body.appendChild(tbl);
+      }
+      tableCreate();
+    }
 
-  tableCreate();
+  }
+  sendReq ('content_for_web/numDoc');
 
-  sendReq ('index1');
-  sendReq ('index2');
+  console.log ("Done"); 
+
 
 
