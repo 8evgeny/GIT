a0cd0a16809beb2d9c722ce6f261bca1d2839fe3
8evgeny <8evgeny@gmail.com>
2023-10-18 11:38:51 +0300

    Начат 17 проект createNiokrArkhivDoc

---
+++ 17_createNiokrArkhivDoc/createNiokrArkhivDoc/src/main.cpp
@@ -0,0 +1,66 @@
+﻿#include "main.h"
+#if 0
+Деплой windows
+cd C:\Qt5\5.15.2\mingw81_64\bin
+windeployqt.exe C:\Users\Professional\Desktop\createNiokrArkhivDoc\createNiokrArkhivDoc.exe
+cd C:\Users\Professional\Desktop\createNiokrArkhivDoc
+createNiokrArkhivDoc.exe C:\Users\Professional\Desktop\temp_arсhive
+Скопировать в папку с exe 2 файла   busybox.exe    grep.exe  из папки bin пакета  w64devkit-master или прописать путь
+C:\w64devkit-master\w64devkit\bin  в path
+Скопировать в папку с exe 2 файла  7z.exe  7z.dll
+Или прописать путь к 7z
+ #endif
+int main(int argc, char *argv[])
+{
+    if (argc == 2) {
+        cout << string{"Передается в качестве параметра путь: "}.append(argv[1]) << endl;
+    }
+    else {
+        cout << "Ошибка запуска. Необходимо передать путь к папке с одной единицей хранения!" << endl;
+        return 0;
+    }
+    const path archiv_path{ argv[1] };
+    const unordered_set<string> pe_extensions{ ".json" };
+    cout << endl <<"Patch for json search: "<<archiv_path << endl;
+    vector<string> vectorJsonFiles;
+    vector<string> errorJsonPatch;
+
+    auto iterator = recursive_directory_iterator{ archiv_path, directory_options::skip_permission_denied };
+    for(const auto& entry : iterator) {
+      try {
+        if(!entry.is_regular_file())
+          continue;
+        const auto& extension = entry.path().extension().string();
+        const auto is_pe = pe_extensions.find(extension) != pe_extensions.end();
+        if(!is_pe)
+          continue;
+        ifstream file{ entry.path() };
+        string patch = entry.path().string();
+        vectorJsonFiles.push_back(patch);
+      } catch(const exception& e) {
+        cerr << "Error reading " << entry.path().string() << ": " << e.what() << endl;
+      }
+    }
+    int error = 0;
+    if (vectorJsonFiles.size()>0)
+    {
+        int num = 0;
+        for (auto & patchJsonFile : vectorJsonFiles){
+            ++num;
+            cout << endl<< num << ": "<< patchJsonFile << endl;
+            if(!parseJSON(patchJsonFile)) //парсинг одного файла JSON
+            {
+                cout << "ОШИБКА JSON ФАЙЛА" << endl;
+                ++error;
+                errorJsonPatch.push_back(patchJsonFile);
+            }
+        }
+    }
+
+    cout<< endl<< "Всего разобрано json файлов: " << vectorJsonFiles.size()
+       << endl << "Ошибок разбора: " << error<< endl;
+    for (auto & patchJsonError : errorJsonPatch)
+        cout <<  patchJsonError << endl;
+
+
+}
