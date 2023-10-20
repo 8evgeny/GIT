b94ba6b5733a8dd33905937c53a936c29ce02684
8evgeny <8evgeny@gmail.com>
2023-10-20 11:15:17 +0300

    ncurses  ++

--- 17_createNiokrArkhivDoc/createNiokrArkhivDoc/src/main.cpp
+++ 17_createNiokrArkhivDoc/createNiokrArkhivDoc/src/main.cpp
@@ -19,53 +19,92 @@ https://ru.stackoverflow.com/questions/1263580/ncurses-ввод-и-вывод-р
  #endif
 map<string,string> allData{};
 
-bool enter(string qw)
+bool enter(uint num, string qw)
 {
     initscr();
-    printw(qw.c_str());
+    mvprintw(num*2-2, 0, qw.c_str());
     initscr();
     noecho();
-//    printw("Введите y  или  n !!!\n");
-
     halfdelay(100);         //Устанавливаем ограничение по времени ожидания getch() в 10 сек
-
     bool ret = true;
     bool ex = false;
-    wint_t c;
-    while ( !ex )
-    {
-
-        int rc = get_wch(&c);
+    uint numCharEntered = 0;
+    wint_t tmp;
+    wint_t c1;
+    wint_t c2;
+    wint_t c3;
+    while ( !ex ) {
+        int rc = get_wch(&tmp);
         if (rc==OK) {
-          mvprintw(1, 0, "Ответ: %lc\n", c);
+          ++numCharEntered;
         }
+        switch (numCharEntered) {
+        case 1:
+            c1 = tmp;
+            if ((c1 != L'д') && (c1 != L'н')){
+            mvprintw(num*2-2, 0, qw.c_str());
+            mvprintw(num*2-1, 0, "Введите да  или  нет");
+            numCharEntered = 0;
+            }
+            if ((c1 == L'д') || (c1 == L'н')){
+            mvprintw(num*2-2, 0, qw.c_str());
+            mvprintw(num*2-1, 0, "                    \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+            if (c1 == L'д')
+                mvprintw(num*2-1, 0, "д");
+            if (c1 == L'н')
+                mvprintw(num*2-1, 0, "н");
+            }
+            break;
 
-        if ((c == L'д')||(c == 'y')){
+        case 2:
+            c2 = tmp;
+            if ((c1 == L'д') && (c2 == L'а')){
+                mvprintw(num*2-1, 1, "а");
             ex = true;
-            ret = true;
-        }
-        if (c == 'n'){
+            }
+            if ((c1 == L'д') && (c2 != L'а')){
+            mvprintw(num*2-2, 0, qw.c_str());
+            mvprintw(num*2-1, 0, "Введите да  или  нет");
+            numCharEntered = 0;
+            }
+            if ((c1 == L'н') && (c2 == L'е')){
+            mvprintw(num*2-1, 1, "е");
+            }
+            break;
+        case 3:
+            c3 = tmp;
+            if ((c1 == L'н') && (c2 == L'е') && (c3 == L'т')){
             ex = true;
             ret = false;
+            }
+            if ((c1 == L'н') && (c2 != L'e')){
+            mvprintw(num*2-2, 0, qw.c_str());
+            mvprintw(num*2-1, 0, "Введите да  или  нет");
+            numCharEntered = 0;
+            }
+            if ((c1 == L'н') && (c2 == L'e') && (c3 != L'т')){
+            mvprintw(num*2-2, 0, qw.c_str());
+            mvprintw(num*2-1, 0, "Введите да  или  нет");
+            numCharEntered = 0;
+            }
+            break;
+        default:
+            break;
         }
-
-
     }
-
-        refresh(); //Выводим на настоящий экран
-
-       endwin();
+    refresh(); //Выводим на настоящий экран
+    endwin();
  return ret;
 }
 
 bool answers(){
-    if (!enter(string("1. В ИУЛ в графе Обозначение документа содержится надпись ").append(allData["oboznachenieIkodDokumenta"]).append(" ? \n")))
+    if (!enter(1, string("1. В ИУЛ в графе Обозначение документа содержится надпись ").append(allData["oboznachenieIkodDokumenta"]).append(" ? \n")))
         return false;
-     if (!enter(string("2. В ИУЛ в графе Наименование изделия содержится надпись ").append(allData["naimenovanieIzdeliya"]).append(" ? \n")))
+    if (!enter(2, string("2. В ИУЛ в графе Наименование изделия содержится надпись ").append(allData["naimenovanieIzdeliya"]).append(" ? \n")))
         return false;
-    if (!enter(string("3. В ИУЛ в графе Наименование документа содержится надпись ").append(allData["naimenovanieDokumenta"]).append(" ? \n")))
+    if (!enter(3, string("3. В ИУЛ в графе Наименование документа содержится надпись ").append(allData["naimenovanieDokumenta"]).append(" ? \n")))
         return false;
-     if (!enter(string("4. В ИУЛ в графе Номер последнего изменения указан номер ").append(allData["changeNumStr"]).append(" ? \n")))
+    if (!enter(4, string("4. В ИУЛ в графе Номер последнего изменения указан номер ").append(allData["changeNumStr"]).append(" ? \n")))
         return false;
      return true;
 }
