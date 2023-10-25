e21ea33080c8e9018ba1a4b32535456ebde7e6d4
8evgeny <8evgeny@gmail.com>
2023-10-25 13:23:03 +0300

    +

--- 18_create_soft_links/create_soft_links/src/main.cpp
+++ 18_create_soft_links/create_soft_links/src/main.cpp
@@ -34,12 +34,14 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-    if (engine.rootObjects().isEmpty())
-        return -1;
-
-
-
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+    &app, [url](QObject * obj, const QUrl & objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    QQmlContext *context = engine.rootContext();
+    engine.load(url);
 
 
 
