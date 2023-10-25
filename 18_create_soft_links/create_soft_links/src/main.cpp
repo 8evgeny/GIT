8d002e01ce0e7863b90c0cfddbf2cf178b7182e7
8evgeny <8evgeny@gmail.com>
2023-10-25 12:17:23 +0300

    start 18_project

---
+++ 18_create_soft_links/create_soft_links/src/main.cpp
@@ -0,0 +1,47 @@
+﻿#include "main.h"
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+#if 0
+
+ #endif
+
+void logging()
+{
+
+}
+
+map<string, string> readConfig(const char* conf_file) {
+    map<string, string> config{};
+    cout<<"reading config.ini file\n"<<endl;
+    po::variables_map vm;
+    po::options_description patches("patchesToDirectories");
+    patches.add_options()
+          ("patchesToDirectories.niokrPoOboznacheniyam", po::value<string>(), "")
+          ("patchesToDirectories.niokrFoldersToSoftLinks", po::value<string>(), "");
+  po::options_description desc("Allowed options");
+  desc.add(patches);
+  try {
+    po::parsed_options parsed = po::parse_config_file<char>(conf_file, desc, true);  //флаг true разрешает незарегистрированные опции !
+    po::store(parsed, vm);
+  } catch (const po::reading_file& e) {
+    std::cout << "Error: " << e.what() << std::endl;
+  }
+  po::notify(vm);
+  config["niokrPoOboznacheniyam"] = vm["patchesToDirectories.niokrPoOboznacheniyam"].as<string>();
+  config["niokrFoldersToSoftLinks"] = vm["patchesToDirectories.niokrFoldersToSoftLinks"].as<string>();
+  return config;
+}
+
+int main(int argc, char *argv[])
+{
+
+    auto config = readConfig("../config.ini");
+    cout << "niokrPoOboznacheniyam: \t" << config["niokrPoOboznacheniyam"] << endl;
+    cout << "niokrFoldersToSoftLinks: \t" << config["niokrFoldersToSoftLinks"] << endl<<endl;
+
+
+
+
+
+return 0;
+}
---
+++ 18_create_soft_links/create_soft_links/src/main.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <QFile>
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <filesystem>
+#include <fstream>
+#include <unordered_set>
+
+#define BUFSIZE 300000
+
+using namespace std;
+using namespace chrono_literals;
+using namespace std::filesystem;
+
