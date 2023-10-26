3474552a78adea76bba823742f6457f92dbe1314
8evgeny <8evgeny@gmail.com>
2023-10-26 11:54:13 +0300

    Сохранение перед удалением примеров

--- 18_create_soft_links/create_soft_links/src/widget.h
+++ 18_create_soft_links/create_soft_links/src/widget.h
@@ -21,24 +21,19 @@ public:
     void addPdfItemsToList();
 
 private slots:
-    void on_pushButton_clicked();
-    void on_pushButton_2_clicked();
-    void on_pushButton_3_clicked();
-    void on_pushButton_4_clicked();
-    void on_listWidget_itemChanged(QListWidgetItem *item);
+//    void on_pushButton_clicked();
+//    void on_pushButton_2_clicked();
+//    void on_pushButton_3_clicked();
+//    void on_pushButton_4_clicked();
+//    void on_pushButton_5_clicked();
+//    void on_pushButton_6_clicked();
+//    void on_pushButton_7_clicked();
+//    void on_pushButton_8_clicked();
+//    void on_listWidget_itemChanged(QListWidgetItem *item);
+    void on_pushButton_Save_clicked();
+    void on_pushButton_Load_clicked();
     void on_listWidget_itemDoubleClicked(QListWidgetItem *item);
-
-    void on_pushButton_5_clicked();
-
-    void on_pushButton_6_clicked();
-
-    void on_pushButton_7_clicked();
-
-    void on_pushButton_8_clicked();
-
-    void on_pushButton_9_clicked();
-
-    void on_pushButton_10_clicked();
+    void on_listWidget_2_itemDoubleClicked(QListWidgetItem *item);
 
 private:
     Ui::Widget *ui;
