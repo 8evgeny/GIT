acf1eaaf6d773865ce712499b84b065152fddd2c
8evgeny <8evgeny@gmail.com>
2023-11-22 10:40:05 +0300

    +

--- 20_ISDN_GITLAB/myWork_2_04/2_04_Linux/flash.sh
+++ 20_ISDN_GITLAB/myWork_2_04/2_04_Linux/flash.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 #st-flash erase 
-#st-flash --reset write build_release/20_Build_code_Krasnov.bin 0x8000000
+st-flash --reset write build_release/20_Build_code_Krasnov.bin 0x8000000
 
-st-flash --reset write  ../2_04_Keil/MDK-ARM/ObjectFiles/R_1_DTS_011M_STM32F429ZG.hex 0x8000000
+#st-flash --reset write  ../2_04_Keil/MDK-ARM/ObjectFiles/R_1_DTS_011M_STM32F429ZG.hex 0x8000000
