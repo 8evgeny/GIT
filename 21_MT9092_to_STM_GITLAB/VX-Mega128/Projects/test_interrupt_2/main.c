111d62ecbc425931c1a49365e9e45691a7f0e001
8evgeny <8evgeny@gmail.com>
2023-12-13 10:32:45 +0300

    Задержки убраны  процессор не простаивает

--- 21_MT9092_to_STM_GITLAB/VX-Mega128/Projects/test_interrupt_2/main.c
+++ 21_MT9092_to_STM_GITLAB/VX-Mega128/Projects/test_interrupt_2/main.c
@@ -4,14 +4,14 @@
 #include <avr/interrupt.h>
 
 static void SetupTIMER1 (void);
-
+uint8_t flagPortB;
 void SetupTIMER1 (void){
    //На частоте тактов 16 МГц прерывание переполнения T/C1
    // произойдет через (счет до 65535):
    // 1 << CS10 4096 mkS (нет прескалера Fclk)
    // 1 << CS11 32.768 mS (Fclk/8)
    // (1 << CS11)|(1 << CS10) 262.144 mS (Fclk/64)
-   // 1 << CS12 1048.576 mS (Fclk/256)
+//    1 << CS12 1048.576 mS (Fclk/256)
    TCCR1B = (1 << CS12);
    TCNT1 = 65536-62439;        //коррекция счетчика, чтобы время было ровно 1 секунда
 
@@ -19,17 +19,26 @@ void SetupTIMER1 (void){
    TIMSK = (1 << TOIE1);
 }
 
-ISR (TIMER1_OVF_vect, ISR_BLOCK){
+ISR (TIMER1_OVF_vect){
 
     //теперь прерывание будет происходить через 62439 тиков
     // таймера 1, что на частоте 16 МГц составит 1 сек.
     TCNT1 = 65536-62439;
     //Далее идет код, который будет работать каждую секунду.
     //Желательно, чтобы этот код был короче.
-    PORTB = 0b00000011;
-    _delay_ms(50);
-    PORTB = 0b00000000;
-    _delay_ms(50);
+    if (flagPortB == 0)
+    {
+        PORTB = 0b00000011;
+        flagPortB = 1;
+    }
+    else
+    {
+        PORTB = 0b00000000;
+        flagPortB = 0;
+    }
+//    _delay_ms(50);
+//    PORTB = 0b00000000;
+//    _delay_ms(50);
 
 }
 
