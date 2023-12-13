700fc42bad0e3d031ec6b5502c3f519d0e832a52
8evgeny <8evgeny@gmail.com>
2023-12-13 16:24:24 +0300

    Прерывания от кнопки работают

--- 21_MT9092_to_STM_GITLAB/VX-Mega128/Projects/test_interrupt_3/main.c
+++ 21_MT9092_to_STM_GITLAB/VX-Mega128/Projects/test_interrupt_3/main.c
@@ -2,40 +2,64 @@
 #define F_CPU 14745600UL
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#define PIN_INT0 PD0
+uint8_t flagButton;
 //Процессор полностью свободен - прерывания от двух таймеров
 //Добавляем прерывания от кнопки
-static void SetupTIMER1 (void);
-static void SetupTIMER3 (void);
+void SetupTIMER1 (void);
+void SetupTIMER3 (void);
+void SetupInt0 (void);
 
 void SetupTIMER1 (void){
     TCCR1B |= (1 << CS12);  //Fclk/256
     TCCR1B |= (1 << CS10);  // если добавить то - Fclk/1024
-    TCNT1 = 65536-62439;    //коррекция счетчика, чтобы время было ровно 1 секунда
-    TIMSK = (1 << TOIE1);   // Разрешение прерывания overflow таймера 1
+//    TCNT1 = 65536-62439;    //коррекция счетчика, чтобы время было ровно 1 секунда
+    TCNT1 = 65535; //Первое срабатывание сразу
+    TIMSK |= (1 << TOIE1);   // Разрешение прерывания overflow таймера 1
 }
 
 void SetupTIMER3 (void){
     TCCR3B |= (1 << CS32);  //Fclk/256
-    ETIMSK = (1 << TOIE3);  // Разрешение прерывания overflow таймера 3
+    ETIMSK |= (1 << TOIE3);  // Разрешение прерывания overflow таймера 3
+}
+
+void SetupInt0 (void){
+    EIMSK |=  (1<<INT0);    //Разрешаем внешнее прерывание INT0
+    EICRA |= (1<<ISC01)|(0<<ISC00);  //настраиваем условие прерывания
 }
 
 ISR (TIMER1_OVF_vect){
-    TCNT1 = 20000;          //Чем число ближе к 65535  тем быстрее сработает таймер 1 (LED ON)
-    PORTB = 0b00000011;
+    TCNT1 = 45000;          //Чем число ближе к 65535  тем быстрее сработает таймер 1 (LED ON)
+    PORTB |= 0b00000001;
     TCNT3 = 65000;          //Чем число ближе к 65535  тем быстрее сработает таймер 3 (LED OFF)
 }
 
 ISR (TIMER3_OVF_vect) {
-    PORTB = 0b00000000;
+        PORTB &= 0b11111110;
+    }
+
+ISR (INT0_vect) {
+    switch (flagButton) {
+    case 0: flagButton = 1; PORTB |= 0b00000010;
+        break;
+    case 1: flagButton = 0; PORTB &= 0b11111101;
+        break;
+    default:
+        break;
+    }
 }
 
 int main(void)
 {
     DDRB |= ( 1 << 0 );     //PB0 PB1  как выходы
     DDRB |= ( 1 << 1 );
-    DDRB &= ~( 1 << 2 );    //PB2  как вход
+    DDRD &= ~(1<<PIN_INT0);    //PD0  как вход
+    //включаем подтягивающий резистор
+    PORTD |= (1<<PIN_INT0);
     SetupTIMER1();
     SetupTIMER3();
+    SetupInt0();
+    PORTB = 0b00000011;
     sei();
     while (1)
     {
