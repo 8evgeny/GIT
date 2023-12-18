bff0e4fe515616c18637f2b9a4f53d1743597851
8evgeny <8evgeny@gmail.com>
2023-12-18 13:20:37 +0300

    +

--- 21_MT9092_to_STM_GITLAB/VX-Mega128/STM407_as_BASE/Core/Src/main.c
+++ 21_MT9092_to_STM_GITLAB/VX-Mega128/STM407_as_BASE/Core/Src/main.c
@@ -106,8 +106,8 @@ HAL_UART_Transmit(&huart6,(uint8_t*)"Test_UART\r\n", sizeof ("Test_UART\r\n") -1
   {
       char tmp[16]="";
 //      char bufSPI[16]="";
-      while (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4) == GPIO_PIN_SET ){} //Ожидаем PSI_SS
-      HAL_StatusTypeDef result =  HAL_SPI_Receive(&hspi1,(uint8_t*)tmp, 1, 0xFFFFFFFF);
+//      while (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4) == GPIO_PIN_SET ){} //Ожидаем PSI_SS
+      HAL_StatusTypeDef result =  HAL_SPI_Receive(&hspi1,(uint8_t*)tmp, 8, 0xFFFFFFFF);
       if (result == HAL_OK)
       {
 //          tmp[16] = '\r';
@@ -115,8 +115,8 @@ HAL_UART_Transmit(&huart6,(uint8_t*)"Test_UART\r\n", sizeof ("Test_UART\r\n") -1
 //          sprintf(bufSPI, "%.64s", tmp);
 
 //          if (flagDmaSend == 1) {
-      while (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4) == GPIO_PIN_RESET){}
-              HAL_UART_Transmit(&huart6,(uint8_t*)tmp, 1, 1000);
+//      while (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4) == GPIO_PIN_RESET){}
+              HAL_UART_Transmit(&huart6,(uint8_t*)tmp, 8, 1000);
 //              flagDmaSend = 0;
 //          }
       }
@@ -196,7 +196,7 @@ static void MX_SPI1_Init(void)
   hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
   hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.NSS = SPI_NSS_HARD_INPUT;
   hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
@@ -267,17 +267,10 @@ static void MX_DMA_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
-
-  /*Configure GPIO pin : PA4 */
-  GPIO_InitStruct.Pin = GPIO_PIN_4;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
 
