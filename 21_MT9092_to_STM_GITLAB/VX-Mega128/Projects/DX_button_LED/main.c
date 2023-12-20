c06d0a485d4d804738e5465f45339de6d2e3a999
8evgeny <8evgeny@gmail.com>
2023-12-20 18:04:55 +0300

    FIX  рычаг  зажигает и гасит  LED

--- 21_MT9092_to_STM_GITLAB/VX-Mega128/Projects/DX_button_LED/main.c
+++ 21_MT9092_to_STM_GITLAB/VX-Mega128/Projects/DX_button_LED/main.c
@@ -10,15 +10,18 @@ int main(void)
     DDRF = 0b0000000; //6 рычагов  PF2 - PF7 (предположительно нулем )
     PORTF = 0b11111100;  //Подтяжка к 1
     PORTC = 0b11111111;
-     while (1)
-     {
-         if (!(PINF & 0b10000000)) //Button 6
-         {
-             PORTC = 0b01111111;
+     while (1){
+         if (!(PINF & 0b10000000)){ //Button 6  загорается  - нулем
+            PORTC &= 0b01111111;
          }
-         if (!(PINF & 0b01000000)) //Button 5
-         {
-             PORTC = 0b10111111;
+         if (PINF | 0b10000000){ //Button 6  гаснет  - единицей
+            PORTC |= 0b10000000;
+         }
+         if (!(PINF & 0b01000000)){ //Button 5
+            PORTC &= 0b10111111;
+         }
+         if (PINF | 0b01000000){
+            PORTC |= 0b01000000;
          }
      }
 }
