dd5ce903ca14a83eb802b46e7c8ecbdce569c629
8evgeny <8evgeny@gmail.com>
2024-01-01 14:26:23 +0300

    Код С прерывания от 2 таймеров

--- 21_MT9092_to_STM_GITLAB/VX-Mega128/Projects/test_asm_3/ignore.c
+++ 21_MT9092_to_STM_GITLAB/VX-Mega128/Projects/test_asm_3/ignore.c
@@ -1,39 +1,51 @@
 #include <avr/io.h>
-#include <avr/iom128.h>
 #define F_CPU 14745600UL
 #include <util/delay.h>
-//#include <avr/interrupt.h>
+#include <avr/interrupt.h>
+#define PIN_INT0 PD0
+//Процессор полностью свободен - прерывания от двух таймеров
+void SetupTIMER1 (void);
+void SetupTIMER3 (void);
+void SetupInt0 (void);
 
-void sleep(void);
-int main(void)
-{
-//    DDRD |= ( 1 << 1 );
-//    DDRD |= ( 1 << 2 );
-//    DDRD &= ~( 1 << 0 );  //PD0  как вход
-
-    DDRD = 0b00000110; //Можно и так
-     while (1)
-     {
-//         if( ~(PIND | ~0x01) )//PD0 нажата   срабатывает с задержкой  пока не кончится цикл _delay_ms(2000) нужно прерывание
-//         {
-//             PORTD = 0b00000110;
-//             _delay_ms(100);
-//             PORTD = 0b00000000;
-//             _delay_ms(100);
-//         }
-//         else
-//         {
-             PORTD = 0b00000110;
-             sleep();
-             PORTD = 0b00000000;
-             sleep();;
-//         }
-
-     }
-//    PORTD = 0b00000100;
+void SetupTIMER1 (void){
+    TCCR1B |= (1 << CS12);  //Fclk/256
+    TCCR1B |= (1 << CS10);  // если добавить то - Fclk/1024
+//    TCNT1 = 65536-62439;    //коррекция счетчика, чтобы время было ровно 1 секунда
+    TCNT1 = 65535; //Первое срабатывание сразу
+    TIMSK |= (1 << TOIE1);   // Разрешение прерывания overflow таймера 1
 }
 
-void sleep(void)
+void SetupTIMER3 (void){
+    TCCR3B |= (1 << CS32);  //Fclk/256
+    ETIMSK |= (1 << TOIE3);  // Разрешение прерывания overflow таймера 3
+}
+
+void SetupInt0 (void){
+    EIMSK |=  (1<<INT0);    //Разрешаем внешнее прерывание INT0
+    EICRA |= (1<<ISC01)|(0<<ISC00);  //настраиваем условие прерывания
+}
+
+ISR (TIMER1_OVF_vect){
+    TCNT1 = 45000;          //Чем число ближе к 65535  тем быстрее сработает таймер 1 (LED ON)
+    PORTD |= 0b00000110;
+    TCNT3 = 65000;          //Чем число ближе к 65535  тем быстрее сработает таймер 3 (LED OFF)
+}
+
+ISR (TIMER3_OVF_vect) {
+        PORTD &= 0b11111001;
+    }
+
+int main(void)
 {
-    _delay_ms(1000);
+    DDRD |= ( 1 << 1 );     //PD1 PD2  как выходы
+    DDRD |= ( 1 << 2 );
+
+    SetupTIMER1();
+    SetupTIMER3();
+    PORTD = 0b00000110;
+    sei();
+    while (1)
+    {
+    }
 }
--- 21_MT9092_to_STM_GITLAB/VX-Mega128/Projects/test_interrupt_3/main.c
+++ 21_MT9092_to_STM_GITLAB/VX-Mega128/Projects/test_interrupt_3/main.c
@@ -30,12 +30,12 @@ void SetupInt0 (void){
 
 ISR (TIMER1_OVF_vect){
     TCNT1 = 45000;          //Чем число ближе к 65535  тем быстрее сработает таймер 1 (LED ON)
-    PORTB |= 0b00000001;
+    PORTD |= 0b00000110;
     TCNT3 = 65000;          //Чем число ближе к 65535  тем быстрее сработает таймер 3 (LED OFF)
 }
 
 ISR (TIMER3_OVF_vect) {
-        PORTB &= 0b11111110;
+        PORTD &= 0b11111001;
     }
 
 ISR (INT0_vect) {
@@ -51,15 +51,15 @@ ISR (INT0_vect) {
 
 int main(void)
 {
-    DDRB |= ( 1 << 0 );     //PB0 PB1  как выходы
-    DDRB |= ( 1 << 1 );
-    DDRD &= ~(1<<PIN_INT0);    //PD0  как вход
+    DDRD |= ( 1 << 1 );     //PB0 PB1  как выходы
+    DDRD |= ( 1 << 2 );
+//    DDRD &= ~(1<<PIN_INT0);    //PD0  как вход
     //включаем подтягивающий резистор
-    PORTD |= (1<<PIN_INT0);
+//    PORTD |= (1<<PIN_INT0);
     SetupTIMER1();
     SetupTIMER3();
-    SetupInt0();
-    PORTB = 0b00000011;
+//    SetupInt0();
+    PORTD = 0b00000110;
     sei();
     while (1)
     {
