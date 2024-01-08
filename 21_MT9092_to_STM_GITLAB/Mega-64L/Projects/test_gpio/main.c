c10376adc0b7856f3cba7d40ae34c9fc6cf50849
8evgeny <8evgeny@gmail.com>
2024-01-08 10:58:16 +0300

    Project  Mega64  led1 - 4

---
+++ 21_MT9092_to_STM_GITLAB/Mega-64L/Projects/test_gpio/main.c
@@ -0,0 +1,25 @@
+#include <avr/io.h>
+#include <avr/iom64.h>
+#define F_CPU 1000000UL  //так как от внутреннего ренератора
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+int main(void)
+{
+    DDRE |= ( 1 << 4 );
+    DDRE |= ( 1 << 5 );
+    DDRE |= ( 1 << 6 );
+    DDRE |= ( 1 << 7 );
+//    DDRE = 0b11110000; //Можно и так
+     while (1)
+     {
+             PORTE = 0b00010000;
+             _delay_ms(100);
+             PORTE = 0b00100000;
+             _delay_ms(200);
+             PORTE = 0b01000000;
+             _delay_ms(300);
+             PORTE = 0b10000000;
+             _delay_ms(400);
+     }
+}
