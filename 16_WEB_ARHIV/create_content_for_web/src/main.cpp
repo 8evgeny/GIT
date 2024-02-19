1ac406a0891d10497a10805e9574c43c5b7fb785
8evgeny <8evgeny@gmail.com>
2024-02-19 20:13:14 +0300

    Дома поломалось

--- 16_WEB_ARHIV/create_content_for_web/src/main.cpp
+++ 16_WEB_ARHIV/create_content_for_web/src/main.cpp
@@ -2,16 +2,16 @@
 #include <pqxx/connection>
 #include <pqxx/transaction>
 #include <QThread>
-#include "Poco/Data/Session.h"
-#include "Poco/Data/PostgreSQL/Connector.h"
+//#include "Poco/Data/Session.h"
+//#include "Poco/Data/PostgreSQL/Connector.h"
 #include <vector>
 #include <iostream>
 
-#include <Poco/ActiveRecord/Context.h>
-#include <Poco/Data/PostgreSQL/PostgreSQLException.h>
-using namespace Poco::Data::Keywords;
-using Poco::Data::Session;
-using Poco::Data::Statement;
+//#include <Poco/ActiveRecord/Context.h>
+//#include <Poco/Data/PostgreSQL/PostgreSQLException.h>
+//using namespace Poco::Data::Keywords;
+//using Poco::Data::Session;
+//using Poco::Data::Statement;
 struct Person
 {
     std::string name;
@@ -251,53 +251,53 @@ result nontransactionToDB(connection* conn, string req){
     result R( N.exec( req ));/* Execute SQL query */
     return R;
 }
-void testPOCO_Psql(){
-    cout << "testPOCO_Psql " << endl;
-    // register PostgreSQL connector
-    Poco::Data::PostgreSQL::Connector::registerConnector();
-    // create a session
-    Poco::Data::Session session(Poco::Data::PostgreSQL::Connector::KEY, "host=127.0.0.1 user=postgres password=postgres dbname=niokrDB port=5432");
-    try {
-        session << "DROP TABLE IF EXISTS Person", now;
-        // (re)create table
-        session << "CREATE TABLE Person (Name VARCHAR(30), Address VARCHAR, Age INTEGER)", now;
+//void testPOCO_Psql(){
+//    cout << "testPOCO_Psql " << endl;
+//    // register PostgreSQL connector
+//    Poco::Data::PostgreSQL::Connector::registerConnector();
+//    // create a session
+//    Poco::Data::Session session(Poco::Data::PostgreSQL::Connector::KEY, "host=127.0.0.1 user=postgres password=postgres dbname=niokrDB port=5432");
+//    try {
+//        session << "DROP TABLE IF EXISTS Person", now;
+//        // (re)create table
+//        session << "CREATE TABLE Person (Name VARCHAR(30), Address VARCHAR, Age INTEGER)", now;
 
-        // insert some rows
-        Person person =
-        {
-            "Bart Simpson",
-            "Springfield",
-            12
-        };
+//        // insert some rows
+//        Person person =
+//        {
+//            "Bart Simpson",
+//            "Springfield",
+//            12
+//        };
 
-        Statement insert(session);
-        insert << "INSERT INTO Person VALUES($1, $2, $3)",
-            use(person.name),
-            use(person.address),
-            use(person.age);
-        insert.execute();
+//        Statement insert(session);
+//        insert << "INSERT INTO Person VALUES($1, $2, $3)",
+//            use(person.name),
+//            use(person.address),
+//            use(person.age);
+//        insert.execute();
 
-        person.name    = "Lisa Simpson";
-        person.address = "Springfield";
-        person.age     = 10;
+//        person.name    = "Lisa Simpson";
+//        person.address = "Springfield";
+//        person.age     = 10;
 
-        insert.execute();
+//        insert.execute();
 
-    //     a simple query
-        Statement select(session);
-        select << "SELECT Name, Address, Age FROM Person",
-            into(person.name),
-            into(person.address),
-            into(person.age),
-            range(0, 1); //  iterate over result set one row at a time
+//    //     a simple query
+//        Statement select(session);
+//        select << "SELECT Name, Address, Age FROM Person",
+//            into(person.name),
+//            into(person.address),
+//            into(person.age),
+//            range(0, 1); //  iterate over result set one row at a time
 
-        while (!select.done())
-        {
-            select.execute();
-            std::cout << person.name << " " << person.address << " " << person.age << std::endl;
-        }
-        std::cout << std::endl;
-    } catch (Poco::Data::PostgreSQL::StatementException &e) {
-                std::cout << e.displayText()  << std::endl;
-        }
-}
+//        while (!select.done())
+//        {
+//            select.execute();
+//            std::cout << person.name << " " << person.address << " " << person.age << std::endl;
+//        }
+//        std::cout << std::endl;
+//    } catch (Poco::Data::PostgreSQL::StatementException &e) {
+//                std::cout << e.displayText()  << std::endl;
+//        }
+//}
